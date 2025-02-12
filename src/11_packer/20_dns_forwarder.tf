f7b95387c6896598dfe83c9caa9afe3d97ddc13f
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-02-12 12:24:59 +0100

    added depends on

--- src/11_packer/20_dns_forwarder.tf
+++ src/11_packer/20_dns_forwarder.tf
@@ -2,9 +2,13 @@ module "dns_forwarder_image" {
   source = "./.terraform/modules/__v4__/dns_forwarder_vm_image"
 
   prefix              = local.product
-  resource_group_name = data.azurerm_resource_group.vnet_rg.name
+  resource_group_name = azurerm_resource_group.packer_rg.name
   location            = var.location
   image_name          = "${local.project}-dns-forwarder-ubuntu2204-image"
   image_version       = var.dns_forwarder_image_version
   subscription_id     = data.azurerm_subscription.current.subscription_id
+
+  depends_on = [
+    azurerm_resource_group.packer_rg,
+  ]
 }
--- src/11_packer/21_azure_devops_agent.tf
+++ src/11_packer/21_azure_devops_agent.tf
@@ -1,17 +1,14 @@
-resource "azurerm_resource_group" "azdo_rg" {
-  name     = "${local.project}-azdoa-rg"
-  location = var.location
-
-  tags = var.tags
-}
-
 module "azdoa_custom_image" {
   source = "./.terraform/modules/__v4__/azure_devops_agent_custom_image"
 
   prefix              = var.prefix
-  resource_group_name = azurerm_resource_group.azdo_rg.name
+  resource_group_name = azurerm_resource_group.packer_rg.name
   location            = var.location
   image_name          = "${local.project}-azdo-agent-ubuntu2204-image"
   image_version       = var.azdo_agent_image_version
   subscription_id     = data.azurerm_subscription.current.subscription_id
+
+  depends_on = [
+    azurerm_resource_group.packer_rg,
+  ]
 }
