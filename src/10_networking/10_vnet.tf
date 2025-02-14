a760fe85aebe5fe9b2d0f8f0d3bd64c8f08fcb3a
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-02-14 11:18:55 +0100

    vnet hub changed cirds to 10.99.0.0/16

--- src/10_networking/10_vnet.tf
+++ src/10_networking/10_vnet.tf
@@ -19,19 +19,3 @@ module "vnet_core_hub" {
 
   tags = var.tags
 }
-
-#
-# Subnet
-#
-
-# # Azdoa
-# module "azdoa_snet" {
-#   source = "./.terraform/modules/__v3__/subnet"
-#   count  = var.enable_azdoa ? 1 : 0
-#
-#   name                                      = "${local.project}-azdoa-snet"
-#   resource_group_name                       = azurerm_resource_group.rg_vnet.name
-#   virtual_network_name                      = module.vnet_core.name
-#   address_prefixes                          = var.cidr_subnet_azdoa
-#   private_endpoint_network_policies_enabled = true
-# }
