e4ad498c2326dd0ae6d14f56b2229195a1e8f608
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-02-14 10:37:20 +0100

    updated azurerm to v4

--- src/30_core/99_main.tf
+++ src/30_core/99_main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.114"
+      version = "~> 4"
     }
   }
 
@@ -27,7 +27,7 @@ data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
 
-module "__v3__" {
-  # https://github.com/pagopa/terraform-azurerm-v3/releases/tag/v8.73.0
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git?ref=e57ed125b0311e6c2674427bf887437ddc40c548"
+module "__v4__" {
+  # https://github.com/pagopa/terraform-azurerm-v4/releases/tag/v1.4.0
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git?ref=80b6cf16e70fd176ad7de10c4027f9ad791d7c6a"
 }
