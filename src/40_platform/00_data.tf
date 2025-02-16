81832f208942b81a9fff1e33e132980bf24b22f2
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-02-16 16:11:54 +0100

    alpha synthetic

--- src/40_platform/00_data.tf
+++ src/40_platform/00_data.tf
@@ -3,6 +3,15 @@ data "azuread_group" "adgroup_admin" {
   display_name = "${local.product}-adgroup-admin"
 }
 
+#
+# Network
+#
+data "azurerm_virtual_network" "vnet_platform" {
+  name                = local.vnet_core_platform_name
+  resource_group_name = local.vnet_rg_name
+}
+
+
 # ### Log Analytics
 # data "azurerm_log_analytics_workspace" "log_analytics" {
 #   name                = local.log_analytics_workspace_name
--- src/40_platform/99_locals.tf
+++ src/40_platform/99_locals.tf
@@ -3,6 +3,12 @@ locals {
   product          = "${var.prefix}-${var.env_short}"
   product_nodomain = "${var.prefix}-${var.env_short}-${var.location_short}"
 
+  #
+  # Network
+  #
+  vnet_rg_name                = "${local.product_nodomain}-core-spoke-network-rg"
+  vnet_core_platform_name                = "${local.product_nodomain}-core-spoke-platform-vnet"
+
   monitor_resource_group_name  = "${local.product_nodomain}-core-monitor-rg"
   log_analytics_workspace_name = "${local.product_nodomain}-core-law"
 }
