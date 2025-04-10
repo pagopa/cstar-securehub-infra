67f884760c6d6cb739671f8fedd24894c86b811c
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-10 14:40:57 +0200

    feat: update CIDR configurations and add Event Hub subnet module

--- src/domains/idpay-common/01_network.tf
+++ src/domains/idpay-common/01_network.tf
@@ -7,6 +7,15 @@ module "idpay_cosmosdb_snet" {
   address_prefixes     = var.cidr_idpay_data_cosmos
 }
 
+module "idpay_eventhub_snet" {
+  source = "./.terraform/modules/__v4__/subnet"
+
+  name                 = "${local.project}-eventhub-snet"
+  virtual_network_name = local.vnet_spoke_data_name
+  resource_group_name  = local.vnet_spoke_data_rg_name
+  address_prefixes     = var.cidr_idpay_data_eventhub
+}
+
 module "idpay_redis_snet" {
   source = "./.terraform/modules/__v4__/subnet"
 
