4305c6f1cd115baa3b3ba632b445e8b024026148
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-10 11:15:13 +0200

    fix: correct resource lock condition for development environment

--- src/40_platform/01_default_resource_groups.tf
+++ src/40_platform/01_default_resource_groups.tf
@@ -6,7 +6,7 @@ module "default_resource_groups" {
   resource_group_prefix = "${local.product_nodomain}-${each.key}"
   location              = var.location
   tags                  = merge(var.tags, each.value.tags)
-  enable_resource_locks = var.env == "prod" ? false : true # in prod there are policies that create locks for us
+  enable_resource_locks = var.env == "dev" ? false : true # in prod there are policies that create locks for us
 
   additional_resource_groups = can(each.value.additional_resource_groups) ? each.value.additional_resource_groups : []
 }
