fb625bfbd9c7775689a2b04cfd97d95b8ecaae04
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-13 01:50:25 +0200

    init idpay_app

---
+++ src/70_domains/idpay_app/03_serviceaccounts_azure_devops.tf
@@ -0,0 +1,89 @@
+# resource "kubernetes_namespace" "system_domain_namespace" {
+#   metadata {
+#     name = "${var.domain}-system"
+#   }
+# }
+#
+#
+# module "kubernetes_service_account" {
+#   source = "./.terraform/modules/__v3__/kubernetes_service_account"
+#
+#   name      = "azure-devops"
+#   namespace = local.system_domain_namespace
+# }
+#
+# #-------------------------------------------------------------
+#
+# resource "kubernetes_role_binding" "deployer_binding" {
+#   metadata {
+#     name      = "deployer-binding"
+#     namespace = local.domain_namespace
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-deployer"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "azure-devops"
+#     namespace = local.system_domain_namespace
+#   }
+# }
+#
+# resource "kubernetes_role_binding" "system_deployer_binding" {
+#   metadata {
+#     name      = "system-deployer-binding"
+#     namespace = local.system_domain_namespace
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "system-cluster-deployer"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "azure-devops"
+#     namespace = local.system_domain_namespace
+#   }
+# }
+#
+# resource "kubernetes_role_binding" "kube_system_reader_binding" {
+#   metadata {
+#     name      = "kube-system-reader-${var.domain}"
+#     namespace = "kube-system"
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "kube-system-reader"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "azure-devops"
+#     namespace = local.system_domain_namespace
+#   }
+# }
+#
+#
+# #tfsec:ignore:AZU023
+# resource "azurerm_key_vault_secret" "azure_devops_sa_token" {
+#   depends_on   = [module.kubernetes_service_account]
+#   name         = "${local.aks_name}-azure-devops-sa-token"
+#   value        = module.kubernetes_service_account.sa_token
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+#
+# #tfsec:ignore:AZU023
+# resource "azurerm_key_vault_secret" "azure_devops_sa_cacrt" {
+#   depends_on   = [module.kubernetes_service_account]
+#   name         = "${local.aks_name}-azure-devops-sa-cacrt"
+#   value        = module.kubernetes_service_account.sa_ca_cert
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+#
+#
---
+++ src/70_domains/idpay_app/09_initiative_storage.tf
@@ -0,0 +1,101 @@
+# #
+# # Storage for Initiative Data (i.e. Logo)
+#
+#
+# #tfsec:ignore:azure-storage-default-action-deny
+# module "idpay_initiative_storage" {
+#
+#   source = "./.terraform/modules/__v3__/storage_account"
+#
+#   name                            = replace("${var.domain}${var.env_short}-initiative-storage", "-", "")
+#   account_kind                    = "StorageV2"
+#   account_tier                    = "Standard"
+#   account_replication_type        = var.storage_account_replication_type
+#   access_tier                     = "Hot"
+#   blob_versioning_enabled         = var.storage_enable_versioning
+#   resource_group_name             = azurerm_resource_group.rg_refund_storage.name
+#   location                        = var.location
+#   advanced_threat_protection      = var.storage_advanced_threat_protection
+#   allow_nested_items_to_be_public = false
+#
+#   blob_delete_retention_days    = var.storage_delete_retention_days
+#   public_network_access_enabled = var.storage_public_network_access_enabled
+#
+#   tags = var.tags
+# }
+#
+# resource "azurerm_storage_container" "idpay_logo_container" {
+#   name                  = "logo"
+#   storage_account_name  = module.idpay_initiative_storage.name
+#   container_access_type = "blob"
+# }
+#
+# resource "azurerm_storage_container" "idpay_ranking_container" {
+#   name                  = "ranking"
+#   storage_account_name  = module.idpay_initiative_storage.name
+#   container_access_type = "private"
+# }
+#
+# resource "azurerm_private_endpoint" "idpay_initiative_storage_private_endpoint" {
+#
+#   name                = "${local.product}-initiative-storage-private-endpoint"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg_refund_storage.name
+#   subnet_id           = data.azurerm_subnet.private_endpoint_subnet.id
+#
+#   private_dns_zone_group {
+#     name                 = "${local.product}-initiative-storage-private-dns-zone-group"
+#     private_dns_zone_ids = [data.azurerm_private_dns_zone.storage_account.id]
+#   }
+#
+#   private_service_connection {
+#     name                           = "${local.product}-initiative-storage-private-service-connection"
+#     private_connection_resource_id = module.idpay_initiative_storage.id
+#     is_manual_connection           = false
+#     subresource_names              = ["blob"]
+#   }
+#
+#   tags = var.tags
+#
+#   depends_on = [
+#     module.idpay_initiative_storage
+#   ]
+# }
+#
+# # storage access key
+#
+# resource "azurerm_key_vault_secret" "initiative_storage_access_key" {
+#   name         = "initiative-storage-access-key"
+#   value        = module.idpay_initiative_storage.primary_access_key
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+#
+# #tfsec:ignore:azure-keyvault-ensure-secret-expiry
+# resource "azurerm_key_vault_secret" "initiative_storage_connection_string" {
+#   name         = "initiative-storage-connection-string"
+#   value        = module.idpay_initiative_storage.primary_connection_string
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+#
+# #tfsec:ignore:azure-keyvault-ensure-secret-expiry
+# resource "azurerm_key_vault_secret" "initiative_storage_blob_connection_string" {
+#   name         = "initiative-storage-blob-connection-string"
+#   value        = module.idpay_initiative_storage.primary_blob_connection_string
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+#
+# resource "azurerm_role_assignment" "initiative_storage_data_contributor" {
+#   scope                = module.idpay_initiative_storage.id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = data.azurerm_api_management.apim_core.identity[0].principal_id
+#
+#   depends_on = [
+#     module.idpay_initiative_storage
+#   ]
+# }
