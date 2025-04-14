db67e9663097baa65aaf72e7b1a5a62d570872ad
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-14 12:45:50 +0200

    added providers kubernetes and helm

--- src/70_domains/idpay_app/99_main.tf
+++ src/70_domains/idpay_app/99_main.tf
@@ -14,6 +14,14 @@ terraform {
       source  = "hashicorp/external"
       version = "~> 2.3"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.36.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17.0"
+    }
   }
 
   backend "azurerm" {}
