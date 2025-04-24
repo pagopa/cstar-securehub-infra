c9fa656f58b776aae0c17183a489ab65f2bcc086
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-24 17:21:25 +0200

    minor fix

--- src/70_domains/idpay_app/00_key_vault.tf
+++ src/70_domains/idpay_app/00_key_vault.tf
@@ -1,3 +1,34 @@
+#
+# KeyVault
+#
+data "azurerm_key_vault" "key_vault_domain" {
+  name                = local.idpay_kv_name
+  resource_group_name = local.idpay_kv_rg_name
+}
+
+### ARGO
+data "azurerm_key_vault_secret" "argocd_admin_username" {
+  name         = "argocd-admin-username"
+  key_vault_id = data.azurerm_key_vault.key_vault_domain.id
+}
+
+data "azurerm_key_vault_secret" "argocd_admin_password" {
+  name         = "argocd-admin-password"
+  key_vault_id = data.azurerm_key_vault.key_vault_domain.id
+}
+
+### WORKLOAD
+data "azurerm_key_vault_secret" "workload_identity_client_id" {
+  name         = "idpay-workload-identity-client-id"
+  key_vault_id = data.azurerm_key_vault.key_vault_domain.id
+}
+
+data "azurerm_key_vault_secret" "workload_identity_service_account_name" {
+  name         = "idpay-workload-identity-service-account-name"
+  key_vault_id = data.azurerm_key_vault.key_vault_domain.id
+}
+
+
 # data "azurerm_key_vault" "kv" {
 #   name                = "${local.product}-${var.domain}-kv"
 #   resource_group_name = "${local.product}-${var.domain}-sec-rg"
