682ea236313d7334b1a977b8a1a71175dec8a579
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-28 14:38:09 +0200

    init

--- src/70_domains/idpay_common/06_service_bus.tf
+++ src/70_domains/idpay_common/06_service_bus.tf
@@ -1,111 +1,142 @@
-resource "azurerm_servicebus_namespace" "idpay-service-bus-ns" {
+locals {
+  servicebus_queues = {
+    idpay-onboarding-request = {
+      requires_duplicate_detection        = true
+      duplicate_detection_history_time_window = "P1D"
+      dead_lettering_on_message_expiration = true
+      authorization_rules = [
+        {
+          name   = "idpay-onboarding-request-producer"
+          listen = false
+          send   = true
+          manage = false
+        },
+        {
+          name   = "idpay-onboarding-request-processor"
+          listen = true
+          send   = true
+          manage = false
+        }
+      ]
+    }
+    idpay-payment-timeout = {
+      requires_duplicate_detection        = true
+      duplicate_detection_history_time_window = "P1D"
+      dead_lettering_on_message_expiration = true
+      authorization_rules = [
+        {
+          name   = "idpay-payment-timeout-consumer"
+          listen = true
+          send   = false
+          manage = false
+        }
+      ]
+    }
+  }
+
+  servicebus_namespace_auth_rules = [
+    {
+      name   = "idpay-service-bus-ns-manager"
+      listen = true
+      send   = true
+      manage = true
+    }
+  ]
+
+  servicebus_queue_auth_rules_map = merge(
+    {
+      for queue_name, queue in local.servicebus_queues :
+      for rule in queue.authorization_rules :
+      "${queue_name}-${rule.name}" => {
+        queue_name = queue_name
+        rule       = rule
+      }
+    }
+  )
+}
+
+resource "azurerm_servicebus_namespace" "idpay_service_bus_ns" {
   name                = "${local.product}-${var.domain}-sb-ns"
   location            = var.location
   resource_group_name = data.azurerm_resource_group.idpay_data_rg.name
   sku                 = var.service_bus_namespace.sku
   minimum_tls_version = "1.2"
+  public_network_access_enabled = false
+  tags = var.tags
+}
+
+resource "azurerm_private_endpoint" "idpay_service_bus_pe" {
+  count               = var.private_endpoint_enabled && length(var.private_dns_zone_servicebus_ids) > 0 ? 1 : 0
+
+  name                = coalesce(var.private_endpoint_servicebus_name, format("%s-private-endpoint-sb", var.name))
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = coalesce(var.private_service_connection_servicebus_name, format("%s-private-endpoint-sb", var.name))
+    private_connection_resource_id = azurerm_servicebus_namespace.idpay_service_bus_ns.id
+    is_manual_connection           = false
+    subresource_names              = ["namespace"]
+  }
+
+  dynamic "private_dns_zone_group" {
+    for_each = var.private_dns_zone_servicebus_ids != null ? ["dummy"] : []
+    content {
+      name                 = "private-dns-zone-group"
+      private_dns_zone_ids = var.private_dns_zone_servicebus_ids
+    }
+  }
 
   tags = var.tags
 }
 
-resource "azurerm_servicebus_namespace_authorization_rule" "idpay-service-bus-ns-manager" {
-  name         = "idpay-service-bus-ns-manager"
-  namespace_id = azurerm_servicebus_namespace.idpay-service-bus-ns.id
+resource "azurerm_servicebus_namespace_authorization_rule" "namespace_rules" {
+  for_each = { for rule in local.servicebus_namespace_auth_rules : rule.name => rule }
 
-  listen = true
-  send   = true
-  manage = true
+  name         = each.value.name
+  namespace_id = azurerm_servicebus_namespace.idpay_service_bus_ns.id
+
+  listen = each.value.listen
+  send   = each.value.send
+  manage = each.value.manage
 }
 
-#QUEUE Onboarding Request
-resource "azurerm_servicebus_queue" "idpay-onboarding-request" {
-  name                                    = "idpay-onboarding-request"
-  namespace_id                            = azurerm_servicebus_namespace.idpay-service-bus-ns.id
-  requires_duplicate_detection            = true
-  duplicate_detection_history_time_window = "P1D"
-  dead_lettering_on_message_expiration    = true
-  #  enable_partitioning = true # default false
+resource "azurerm_servicebus_queue" "queues" {
+  for_each = local.servicebus_queues
+
+  name                                    = each.key
+  namespace_id                            = azurerm_servicebus_namespace.idpay_service_bus_ns.id
+  requires_duplicate_detection            = each.value.requires_duplicate_detection
+  duplicate_detection_history_time_window = each.value.duplicate_detection_history_time_window
+  dead_lettering_on_message_expiration    = each.value.dead_lettering_on_message_expiration
 }
 
-resource "azurerm_servicebus_queue_authorization_rule" "idpay-onboarding-request-producer" {
-  name     = "idpay-onboarding-request-producer"
-  queue_id = azurerm_servicebus_queue.idpay-onboarding-request.id
+resource "azurerm_servicebus_queue_authorization_rule" "queue_auth_rules" {
+  for_each = local.servicebus_queue_auth_rules_map
 
-  listen = false
-  send   = true
-  manage = false
+  name     = each.value.rule.name
+  queue_id = azurerm_servicebus_queue.queues[each.value.queue_name].id
+
+  listen = each.value.rule.listen
+  send   = each.value.rule.send
+  manage = each.value.rule.manage
 }
 
-#processor have to read and send bck the data with a delay if there is the need to reprocess
-resource "azurerm_servicebus_queue_authorization_rule" "idpay-onboarding-request-processor" {
-  name     = "idpay-onboarding-request-processor"
-  queue_id = azurerm_servicebus_queue.idpay-onboarding-request.id
+resource "azurerm_key_vault_secret" "namespace_auth_secrets" {
+  for_each = azurerm_servicebus_namespace_authorization_rule.namespace_rules
 
-  listen = true
-  send   = true
-  manage = false
-}
-
-#QUEUE Payment Timeout
-
-resource "azurerm_servicebus_queue" "idpay-payment-timeout" {
-  name                                    = "idpay-payment-timeout"
-  namespace_id                            = azurerm_servicebus_namespace.idpay-service-bus-ns.id
-  requires_duplicate_detection            = true
-  duplicate_detection_history_time_window = "P1D"
-  dead_lettering_on_message_expiration    = true
-  #  enable_partitioning = true # default false
-}
-
-resource "azurerm_servicebus_queue_authorization_rule" "idpay-payment-timeout-consumer" {
-  name     = "idpay-payment-timeout-consumer"
-  queue_id = azurerm_servicebus_queue.idpay-payment-timeout.id
-
-  listen = true
-  send   = false
-  manage = false
-}
-
-#
-# 🔑 KEY-VAULT Secret
-#
-resource "azurerm_key_vault_secret" "idpay-service-bus-ns-manager-sas-key" {
-
-  name         = "idpay-service-bus-ns-manager-sas-key"
-  value        = azurerm_servicebus_namespace_authorization_rule.idpay-service-bus-ns-manager.primary_connection_string
+  name         = each.value.name
+  value        = each.value.primary_connection_string
   content_type = "text/plain"
-
   key_vault_id = data.azurerm_key_vault.domain_kv.id
 }
 
-resource "azurerm_key_vault_secret" "idpay-onboarding-request-producer-sas-key" {
+resource "azurerm_key_vault_secret" "queue_auth_secrets" {
+  for_each = azurerm_servicebus_queue_authorization_rule.queue_auth_rules
 
-  name         = "idpay-onboarding-request-producer-sas-key"
-  value        = azurerm_servicebus_queue_authorization_rule.idpay-onboarding-request-producer.primary_connection_string
+  name         = each.value.name
+  value        = each.value.primary_connection_string
   content_type = "text/plain"
-
   key_vault_id = data.azurerm_key_vault.domain_kv.id
 }
-
-resource "azurerm_key_vault_secret" "idpay-onboarding-request-processor-sas-key" {
-
-  name         = "idpay-onboarding-request-processor-sas-key"
-  value        = azurerm_servicebus_queue_authorization_rule.idpay-onboarding-request-processor.primary_connection_string
-  content_type = "text/plain"
-
-  key_vault_id = data.azurerm_key_vault.domain_kv.id
-}
-
-resource "azurerm_key_vault_secret" "idpay-payment-timeout-consumer-sas-key" {
-
-  name         = "idpay-payment-timeout-consumer-sas-key"
-  value        = azurerm_servicebus_queue_authorization_rule.idpay-payment-timeout-consumer.primary_connection_string
-  content_type = "text/plain"
-
-  key_vault_id = data.azurerm_key_vault.domain_kv.id
-}
-
-
-
-
-
