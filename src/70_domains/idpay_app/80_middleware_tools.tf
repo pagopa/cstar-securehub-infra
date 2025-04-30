85f306a95a3f4402f31275b94ac46540009fbddf
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-30 10:28:13 +0200

    added cert mounter

--- src/70_domains/idpay_app/80_middleware_tools.tf
+++ src/70_domains/idpay_app/80_middleware_tools.tf
@@ -1,49 +1,55 @@
-# module "tls_checker" {
-#   source = "./.terraform/modules/__v3__/tls_checker"
-#
-#   https_endpoint                                            = local.domain_aks_hostname
-#   alert_name                                                = local.domain_aks_hostname
-#   alert_enabled                                             = true
-#   helm_chart_present                                        = true
-#   namespace                                                 = kubernetes_namespace.domain_namespace.metadata[0].name
-#   location_string                                           = var.location_string
-#   kv_secret_name_for_application_insights_connection_string = "appinsights-instrumentation-key"
-#   application_insights_resource_group                       = data.azurerm_resource_group.monitor_rg.name
-#   application_insights_id                                   = data.azurerm_application_insights.application_insights.id
-#   application_insights_action_group_ids                     = [data.azurerm_monitor_action_group.slack.id, data.azurerm_monitor_action_group.email.id]
-#   keyvault_tenant_id                                        = data.azurerm_subscription.current.tenant_id
-#   keyvault_name                                             = data.azurerm_key_vault.kv.name
-#
-#   workload_identity_enabled              = true
-#   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-#   workload_identity_client_id            = module.workload_identity.workload_identity_client_id
-#   depends_on                             = [module.workload_identity]
-# }
-#
-# module "cert_mounter" {
-#   source = "./.terraform/modules/__v3__/cert_mounter"
-#
-#   namespace        = var.domain
-#   certificate_name = var.env_short == "p" ? "${var.aks_cluster_domain_name}-${var.domain}-internal-cstar-pagopa-it" : "${var.aks_cluster_domain_name}-${var.domain}-internal-${var.env}-cstar-pagopa-it"
-#   kv_name          = data.azurerm_key_vault.kv.name
-#   tenant_id        = data.azurerm_subscription.current.tenant_id
-#
-#   workload_identity_enabled              = true
-#   workload_identity_service_account_name = module.workload_identity.workload_identity_service_account_name
-#   workload_identity_client_id            = module.workload_identity.workload_identity_client_id
-#
-#   depends_on = [module.workload_identity]
-# }
-#
-# resource "helm_release" "reloader" {
-#   name       = "reloader"
-#   repository = "https://stakater.github.io/stakater-charts"
-#   chart      = "reloader"
-#   version    = "v1.0.30"
-#   namespace  = kubernetes_namespace.domain_namespace.metadata[0].name
-#
-#   set {
-#     name  = "reloader.watchGlobally"
-#     value = "false"
-#   }
-# }
+### Cert Mounter
+module "cert_mounter" {
+  source = "./.terraform/modules/__v4__/cert_mounter"
+
+  namespace        = var.domain
+  certificate_name = replace(var.ingress_domain_hostname, ".", "-")
+  kv_name          = data.azurerm_key_vault.key_vault_domain.name
+  tenant_id        = data.azurerm_subscription.current.tenant_id
+
+  workload_identity_service_account_name = data.azurerm_key_vault_secret.workload_identity_service_account_name.value
+  workload_identity_client_id            = data.azurerm_key_vault_secret.workload_identity_client_id.value
+  affinity = jsonencode({
+    nodeAffinity = {
+      requiredDuringSchedulingIgnoredDuringExecution = {
+        nodeSelectorTerms = [{
+          matchExpressions = [
+            {
+              key      = "node_type"
+              operator = "In"
+              values   = ["user"]
+            }
+          ]
+        }]
+      }
+    }
+    podAntiAffinity = {
+      preferredDuringSchedulingIgnoredDuringExecution = [{
+        weight = 100
+        podAffinityTerm = {
+          namespaces  = [var.domain]
+          topologyKey = "topology.kubernetes.io/zone"
+          labelSelector = {
+            matchLabels = {
+              "app.kubernetes.io/instance" = "cert-mounter-blueprint"
+            }
+          }
+        }
+      }]
+    }
+  })
+}
+
+# Reloader
+resource "helm_release" "reloader" {
+  name       = "reloader"
+  repository = "https://stakater.github.io/stakater-charts"
+  chart      = "reloader"
+  version    = "v1.0.30"
+  namespace  = var.domain
+
+  set {
+    name  = "reloader.watchGlobally"
+    value = "false"
+  }
+}
