33df3741f93c453b73047e90250e8c9a56f4681b
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2025-05-13 01:11:33 +0200

    breaking: RTD-2612 VPN migrated in Italy (#46)
    * updated modules
    
    * added configuration for VPN
    
    * created vpn gateway in Italy
    
    * fixed legacy peering to use hub vpn gateway
    
    * updated terraform lock
    
    * fix: compute peerings
    
    * module upgrated
    
    * pre-commit fixs

--- src/10_networking/00_data.tf
+++ src/10_networking/00_data.tf
@@ -1,23 +1,32 @@
-# data "azurerm_key_vault" "key_vault" {
-#   name                = "${local.project}-kv"
-#   resource_group_name = "${local.project}-sec-rg"
-# }
 #
-# data "azuread_application" "vpn_app" {
-#   display_name = "${local.product}-app-vpn"
-# }
+# 🔐 KV
+#
+data "azurerm_key_vault" "kv_core" {
+  name                = local.kv_core_name
+  resource_group_name = local.kv_core_resource_group_name
+}
 
-# data "azurerm_resources" "sub_resources" {
-#   type          = "Microsoft.Network/privateDnsZones"
-#   required_tags = local.tags_for_private_dns
-# }
-
+#
+# Vnet Legacy
+#
 data "azurerm_virtual_network" "vnet_weu_core" {
   name                = local.vnet_weu_core.name
   resource_group_name = local.vnet_weu_core.resource_group
 }
 
+data "azurerm_virtual_network" "vnet_weu_integration" {
+  name                = local.vnet_weu_integration.name
+  resource_group_name = local.vnet_weu_integration.resource_group
+}
+
 data "azurerm_virtual_network" "vnet_weu_aks" {
   name                = local.vnet_weu_aks.name
   resource_group_name = local.vnet_weu_aks.resource_group
+}
+
+#
+# Packer
+#
+data "azurerm_resource_group" "rg_packer" {
+  name = local.packer_rg_name
 }
