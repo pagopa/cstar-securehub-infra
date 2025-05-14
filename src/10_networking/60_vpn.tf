5312237472b3b0b1efcae21e99781eddc7bd75fb
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-05-14 11:18:00 +0200

    re-created vmss for dns forwarder into netowrk rg

--- src/10_networking/60_vpn.tf
+++ src/10_networking/60_vpn.tf
@@ -68,7 +68,7 @@ module "dns_forwarder_lb_vmss" {
 
   name                 = local.project
   virtual_network_name = module.vnet_core_hub.name
-  resource_group_name  = data.azurerm_resource_group.rg_packer.name
+  resource_group_name  = azurerm_resource_group.rg_network.name
 
   static_address_lb = cidrhost(var.cidr_subnet_dnsforwarder_lb[0], 4)
   subnet_lb_id      = module.subnet_dns_forwarder_lb.id
