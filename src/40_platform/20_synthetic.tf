3f628405b9ec0b9b04e1588beb785f73afe20f87
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-05-15 00:06:42 +0200

    iterate synthetic from more values

--- src/40_platform/20_synthetic.tf
+++ src/40_platform/20_synthetic.tf
@@ -57,7 +57,12 @@ resource "azurerm_private_endpoint" "private_endpoint_container_app" {
 
 
 module "synthetic_monitoring_jobs" {
-  source = "./.terraform/modules/__v4__/monitoring_function"
+  # source = "./.terraform/modules/__v4__/monitoring_function"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//monitoring_function?ref=fix-monitor-function"
+
+  providers = {
+    grafana = grafana.cloud
+  }
 
   location            = var.location
   prefix              = "${local.product}-${var.location_short}"
@@ -65,8 +70,6 @@ module "synthetic_monitoring_jobs" {
 
   enabled_sythetic_dashboard = true
   subscription_id            = data.azurerm_subscription.current.subscription_id
-  grafana_api_key            = data.azurerm_key_vault_secret.grafana_dashboard_bot_api_key.value
-  grafana_url                = azurerm_dashboard_grafana.grafana_dashboard.endpoint
 
   application_insight_name    = azurerm_application_insights.monitoring_application_insights.name
   application_insight_rg_name = azurerm_application_insights.monitoring_application_insights.resource_group_name
@@ -82,6 +85,7 @@ module "synthetic_monitoring_jobs" {
 
   job_settings = {
     container_app_environment_id = azurerm_container_app_environment.synthetic_cae.id
+    availability_prefix = "syn"
   }
 
   storage_account_settings = {
@@ -103,16 +107,21 @@ module "synthetic_monitoring_jobs" {
 
 locals {
   monitoring_config_raw = concat(
-    yamldecode(templatefile("tae.yaml.tpl", local.synthetic_variables)),
-    yamldecode(templatefile("idpay.yaml.tpl", local.synthetic_variables)),
-    yamldecode(templatefile("shared.yaml.tpl", local.synthetic_variables)),
-    yamldecode(templatefile("mc.yaml.tpl", local.synthetic_variables)),
+    yamldecode(templatefile("${path.module}/synthetic_endpoints/tae.yaml.tpl", local.synthetic_variables)),
+    yamldecode(templatefile("${path.module}/synthetic_endpoints/idpay.yaml.tpl", local.synthetic_variables)),
+    yamldecode(templatefile("${path.module}/synthetic_endpoints/shared.yaml.tpl", local.synthetic_variables)),
+    yamldecode(templatefile("${path.module}/synthetic_endpoints/mc.yaml.tpl", local.synthetic_variables)),
   )
 
   synthetic_variables = {
     tae_enabled = var.synthetic_domain_tae_enabled
-    idpay_enabled = var.synthetic_domain_tae_enabled
-    shared_enabled = var.synthetic_domain_tae_enabled
-    mc_enabled = var.synthetic_domain_tae_enabled
+    idpay_enabled = var.synthetic_domain_idpay_enabled
+    shared_enabled = var.synthetic_domain_shared_enabled
+    mc_enabled = var.synthetic_domain_mc_enabled
+
+    env_name                       = var.env,
+    alert_enabled                  = var.synthetic_alerts_enabled
+    public_hostname           = local.public_hostname
+    internal_private_hostname = local.internal_private_hostname
   }
 }
