a3c54e662a5c5031ab012e6cc2289483967820c0
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2025-05-16 16:57:59 +0200

    add peering from spoke compute to vnet integration

--- src/10_networking/12_peering_legacy.tf
+++ src/10_networking/12_peering_legacy.tf
@@ -16,6 +16,7 @@ locals {
       resource_group_name = data.azurerm_virtual_network.vnet_weu_aks.resource_group_name
     }
   }
+
   # Collect vnet spoke
   all_hub_to_legacy_peerings = {
     for k, v in local.legacy_vnets :
@@ -42,4 +43,22 @@ module "vnet_secure_hub_to_legacy_peerings" {
   target_virtual_network_name      = each.value.name
   target_remote_virtual_network_id = each.value.id
   target_use_remote_gateways       = true
+}
+
+#
+# ⛓️ Peering from secure spoke compute to vnet integration
+#
+module "vnet_secure_spoke_compute_to_vnet_integration" {
+  source = "./.terraform/modules/__v4__/virtual_network_peering"
+
+  # Define source virtual network peering details
+  source_resource_group_name       = azurerm_resource_group.rg_network.name
+  source_virtual_network_name      = module.vnet_spoke_compute.name
+  source_remote_virtual_network_id = module.vnet_spoke_compute.id
+  source_allow_gateway_transit     = true
+
+  target_resource_group_name       = data.azurerm_virtual_network.vnet_weu_integration.resource_group_name
+  target_virtual_network_name      = data.azurerm_virtual_network.vnet_weu_integration.name
+  target_remote_virtual_network_id = data.azurerm_virtual_network.vnet_weu_integration.id
+  target_use_remote_gateways       = false
 }
