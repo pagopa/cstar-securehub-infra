b4fd25cb142a9d547ce04c97559b1c696c32cd6c
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-05-18 13:25:35 +0200

    feat: now azdo image is builded inside azdo rg

--- src/11_packer/21_azure_devops_agent.tf
+++ src/11_packer/21_azure_devops_agent.tf
@@ -1,14 +1,20 @@
+resource "azurerm_resource_group" "azdo" {
+  name     = "${local.product_location}-core-azdo-rg"
+  location = var.location
+  tags     = var.tags
+}
+
 module "azdoa_custom_image" {
   source = "./.terraform/modules/__v4__/azure_devops_agent_custom_image"
 
   prefix              = var.prefix
-  resource_group_name = azurerm_resource_group.packer_rg.name
+  resource_group_name = azurerm_resource_group.azdo.name
   location            = var.location
   image_name          = "${local.project}-azdo-agent-ubuntu2204-image"
   image_version       = var.azdo_agent_image_version
   subscription_id     = data.azurerm_subscription.current.subscription_id
 
   depends_on = [
-    azurerm_resource_group.packer_rg,
+    azurerm_resource_group.azdo,
   ]
 }
