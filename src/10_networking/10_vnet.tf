6a5ae47b91f337a5e32763d7099d741830e7981b
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-06-04 11:41:26 +0200

    clean code

--- src/10_networking/10_vnet.tf
+++ src/10_networking/10_vnet.tf
@@ -53,8 +53,7 @@ module "vnet_spoke_compute" {
 module "vnet_spoke_security" {
   source = "./.terraform/modules/__v4__/virtual_network"
   # source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//virtual_network?ref=vnet-ignore-ddos-protection-plan"
-
-
+  
   name                = "${local.project}-spoke-security-vnet"
   location            = azurerm_resource_group.rg_network.location
   resource_group_name = azurerm_resource_group.rg_network.name
