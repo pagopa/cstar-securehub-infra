8da5ff9dd3788674e6f144cc7ea596a6186d11b7
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2025-06-10 23:48:02 +0200

    feat(idpay): Now use idh module for kv access policy configuration (#65)
    * added idh key vault access policy configurator
    
    * pre-commit fixs
    
    * re-insert sh files
    
    * configured apim managed identity kv config
    
    * pre-commit config fix
    
    * pre-commit config added bash and shell to exclude
    
    * pre-commit fixs
    
    * minor fix
    
    * upgrated modules
    
    * pre-commit fixs
    
    ---------
    
    Co-authored-by: Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>

--- src/70_domains/idpay_security/01_domain_kvs.tf
+++ src/70_domains/idpay_security/01_domain_kvs.tf
@@ -20,64 +20,56 @@ module "key_vault" {
 #
 # KV Policy
 #
-## ad group policy ##
-resource "azurerm_key_vault_access_policy" "admins_group_policy" {
+module "admins_policy" {
+  source = "./.terraform/modules/__v4__/IDH/key_vault_access_policy"
+
   for_each = toset(local.secrets_folders_kv)
 
-  key_vault_id = module.key_vault[each.key].id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azuread_group.adgroup_admin.object_id
-
-  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt", "Backup", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
-  secret_permissions      = ["Get", "List", "Set", "Delete", "Backup", "Purge", "Recover", "Restore"]
-  storage_permissions     = []
-  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover", "Backup", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers"]
+  product_name      = "cstar"
+  idh_resource_tier = "admin" # or developer, external
+  env               = var.env # or prod, uat, etc.
+  key_vault_id      = module.key_vault[each.key].id
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  object_id         = data.azuread_group.adgroup_admin.object_id
 }
 
-## ad group policy ##
-resource "azurerm_key_vault_access_policy" "developers_policy" {
+module "developers_policy" {
+  source = "./.terraform/modules/__v4__//IDH/key_vault_access_policy"
+
   for_each = toset(local.secrets_folders_kv)
 
-  key_vault_id = module.key_vault[each.key].id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azuread_group.adgroup_developers.object_id
-
-  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt", "Rotate", "GetRotationPolicy"]
-  secret_permissions      = ["Get", "List", "Set", "Delete", ]
-  storage_permissions     = []
-  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover"]
-
+  product_name      = "cstar"
+  idh_resource_tier = "developer" # or developer, external
+  env               = var.env     # or prod, uat, etc.
+  key_vault_id      = module.key_vault[each.key].id
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  object_id         = data.azuread_group.adgroup_developers.object_id
 }
 
-resource "azurerm_key_vault_access_policy" "externals_policy" {
+module "externals_policy" {
+  source = "./.terraform/modules/__v4__/IDH/key_vault_access_policy"
+
   for_each = var.env == "dev" ? toset(local.secrets_folders_kv) : []
 
-  key_vault_id = module.key_vault[each.key].id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azuread_group.adgroup_externals.object_id
-
-  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Encrypt", "Decrypt", "Rotate", "GetRotationPolicy"]
-  secret_permissions      = ["Get", "List", "Set", "Delete", ]
-  storage_permissions     = []
-  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover"]
+  product_name      = "cstar"
+  idh_resource_tier = "external" # or developer, external
+  env               = var.env
+  key_vault_id      = module.key_vault[each.key].id
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  object_id         = data.azuread_group.adgroup_externals.object_id
 }
 
 #
 # Managed identities
 #
-resource "azurerm_key_vault_access_policy" "apim_managed_identity" {
+module "apim_managed_identity_policy" {
+  source   = "./.terraform/modules/__v4__/IDH/key_vault_access_policy"
   for_each = toset(local.secrets_folders_kv)
 
-  key_vault_id = module.key_vault[each.key].id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_api_management.apim.identity[0].principal_id
-
-  key_permissions         = ["Get", "List"]
-  secret_permissions      = ["Get", "List", ]
-  storage_permissions     = []
-  certificate_permissions = ["Get", "List", ]
+  product_name      = "cstar"
+  idh_resource_tier = "reader" # or developer, external
+  env               = var.env
+  key_vault_id      = module.key_vault[each.key].id
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  object_id         = data.azurerm_api_management.apim.identity[0].principal_id
 }
