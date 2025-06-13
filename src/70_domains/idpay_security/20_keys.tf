11ccf977b0ae439a497a349fa5c80bea20aa056d
stefano.delia <stefano.delia@pagopa.it>
2025-06-13 17:03:13 +0200

    fix: add pinblock key

--- src/70_domains/idpay_security/20_keys.tf
+++ src/70_domains/idpay_security/20_keys.tf
@@ -8,3 +8,15 @@ resource "azurerm_key_vault_key" "idpay_mil_key" {
     "encrypt"
   ]
 }
+
+resource "azurerm_key_vault_key" "idpay_pinblock_key" {
+  name         = "idpay-pinblock-key"
+  key_vault_id = module.key_vault["idpay"].id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts = [
+    "decrypt",
+    "encrypt"
+  ]
+}
+
