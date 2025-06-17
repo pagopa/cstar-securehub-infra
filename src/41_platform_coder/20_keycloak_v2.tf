fb344b5936b788529d2becd24e2c33c766852a0b
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-06-18 00:10:48 +0200

    minor fix

--- src/41_platform_coder/20_keycloak_v2.tf
+++ src/41_platform_coder/20_keycloak_v2.tf
@@ -4,3 +4,15 @@ resource "kubernetes_namespace" "keycloak" {
   }
 }
 
+resource "kubernetes_config_map" "keycloak-config" {
+  metadata {
+    name      = "keycloak-config"
+    namespace = kubernetes_namespace.keycloak.metadata[0].name
+  }
+
+  data = {
+    kc-db-schema = "public",
+    kc-db-url-port = "5432",
+    kc-db = "postgres"
+  }
+}
