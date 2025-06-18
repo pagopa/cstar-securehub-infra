9acb685326bd57b646423fb80c1307d0865df1c5
diegoitaliait <diego.lagosmorales@pagopa.it>
2025-06-18 11:50:27 +0200

    feat: IDP-2756 idpay now have a more clear workload identity for aks (#91)
    * removed old workload identity
    
    * now all the application have azure client id, as env variable
    
    * pre-commit fixs

--- src/70_domains/idpay_app/00_data.tf
+++ src/70_domains/idpay_app/00_data.tf
@@ -21,3 +21,23 @@ data "azurerm_eventhub_namespace" "eventhub_00" {
   name                = local.eventhub_00_namespace_name
   resource_group_name = local.data_rg_name
 }
+
+#------------------------------------------------------------------
+# Azure AD
+#------------------------------------------------------------------
+# Azure AD
+data "azuread_group" "adgroup_admin" {
+  display_name = "${local.product}-adgroup-admin"
+}
+
+data "azuread_group" "adgroup_developers" {
+  display_name = "${local.product}-adgroup-developers"
+}
+
+data "azuread_group" "adgroup_externals" {
+  display_name = "${local.product}-adgroup-externals"
+}
+
+data "azuread_group" "adgroup_security" {
+  display_name = "${local.product}-adgroup-security"
+}
--- src/70_domains/idpay_common/30_namespace_+_workload_identity.tf
+++ src/70_domains/idpay_common/30_namespace_+_workload_identity.tf
@@ -28,34 +28,6 @@ module "workload_identity_configuration_v2" {
   key_vault_secret_permissions      = ["Get"]
 
   depends_on = [
-    module.workload_identity_v2
+    module.workload_identity_v2,
   ]
-}
-
-
-#
-# Old
-#
-module "workload_identity" {
-  source = "./.terraform/modules/__v4__/kubernetes_workload_identity_init"
-
-  workload_identity_name_prefix         = var.domain
-  workload_identity_resource_group_name = data.azurerm_kubernetes_cluster.aks.resource_group_name
-  workload_identity_location            = var.location
-}
-
-
-module "workload_identity_configuration" {
-  source = "./.terraform/modules/__v4__/kubernetes_workload_identity_configuration"
-
-  workload_identity_name_prefix         = var.domain
-  workload_identity_resource_group_name = data.azurerm_kubernetes_cluster.aks.resource_group_name
-  aks_name                              = data.azurerm_kubernetes_cluster.aks.name
-  aks_resource_group_name               = data.azurerm_kubernetes_cluster.aks.resource_group_name
-  namespace                             = var.domain
-
-  key_vault_id                      = data.azurerm_key_vault.domain_kv.id
-  key_vault_certificate_permissions = ["Get"]
-  key_vault_key_permissions         = ["Get"]
-  key_vault_secret_permissions      = ["Get"]
 }
