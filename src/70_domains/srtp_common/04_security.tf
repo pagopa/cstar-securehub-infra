081d6f3c68aa20403ad41e06aaee8add2dc4d416
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-06-30 23:35:29 +0200

    re-order resources with monitoring

--- src/70_domains/srtp_common/04_security.tf
+++ src/70_domains/srtp_common/04_security.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_secret" "appinisights_connection_string_kv" {
   name         = "appinsights-connection-string"
-  value        = data.azurerm_application_insights.appinsights.connection_string
+  value        = azurerm_application_insights.srtp_application_insights.connection_string
   key_vault_id = data.azurerm_key_vault.domain_kv.id
   tags         = module.tag_config.tags
 }
