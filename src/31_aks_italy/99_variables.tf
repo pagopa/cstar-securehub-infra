7771d3db380724eb5eafd1ed94629af86ff9a747
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-07-04 20:26:46 +0200

    added aks user node pools with IDH

--- src/31_aks_italy/99_variables.tf
+++ src/31_aks_italy/99_variables.tf
@@ -113,27 +113,6 @@ variable "aks_system_node_pool" {
   description = "AKS node pool system configuration"
 }
 
-variable "aks_user_node_pool" {
-  type = object({
-    enabled                    = optional(bool, true),
-    name                       = string,
-    vm_size                    = string,
-    os_disk_type               = string,
-    os_disk_size_gb            = string,
-    node_count_min             = number,
-    node_count_max             = number,
-    node_labels                = map(any),
-    node_taints                = list(string),
-    node_tags                  = map(any),
-    ultra_ssd_enabled          = optional(bool, false),
-    enable_host_encryption     = optional(bool, true),
-    max_pods                   = optional(number, 250),
-    upgrade_settings_max_surge = optional(string, "30%"),
-    zones                      = optional(list(any), [1, 2, 3]),
-  })
-  description = "AKS node pool user configuration"
-}
-
 variable "aks_nodepool_blue" {
   type = object({
     vm_sku_name       = string
@@ -220,11 +199,6 @@ variable "reloader_helm" {
     image_tag     = string
   })
   description = "reloader helm chart configuration"
-}
-
-variable "aks_enable_workload_identity" {
-  type    = bool
-  default = false
 }
 
 variable "skip_metric_validation" {
