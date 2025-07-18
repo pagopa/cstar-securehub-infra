ac4b25e32c43e70857e133604b285b7c1c95c80f
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-07-18 18:07:56 +0200

    feat: [SRTP-733] Create secret slack webhook url (#147)
    * SRTP-733-create-secret-slack-webhook-url-on-rtp-secret
    
    * SRTP-733-create-secret-slack-webhook-url-on-rtp-secret
    
    * SRTP-733 pre commit

--- src/91_github/srtp/00_data.tf
+++ src/91_github/srtp/00_data.tf
@@ -11,3 +11,17 @@ data "azurerm_user_assigned_identity" "cd_client_identity" {
   name                = "${local.project}-github-cd-id"
   resource_group_name = local.cicd_rg_name
 }
+
+# ------------------------------------------------------------------------------
+# General purpose key vault used to protect secrets.
+# ------------------------------------------------------------------------------
+data "azurerm_key_vault" "rtp-kv" {
+  name                = local.rtp_kv_name
+  resource_group_name = local.rtp_kv_resource_group_name
+}
+
+# Key Vault - Slack webhook
+data "azurerm_key_vault_secret" "slack_webhook" {
+  key_vault_id = data.azurerm_key_vault.rtp-kv.id
+  name         = "slack-webhook-url"
+}
