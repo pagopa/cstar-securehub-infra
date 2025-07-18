e01ba45e5bd0104066050a1527db918811e18def
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-07-18 18:06:18 +0200

    SRTP-733 pre commit

--- src/91_github/srtp/99_locals.tf
+++ src/91_github/srtp/99_locals.tf
@@ -1,7 +1,7 @@
 locals {
-  project      = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
-  cicd_rg_name = "${local.project}-cicd-rg"
-  rtp_kv_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-kv"
+  project                    = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}"
+  cicd_rg_name               = "${local.project}-cicd-rg"
+  rtp_kv_name                = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-kv"
   rtp_kv_resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-${var.location_short == "itn" ? "security" : "sec"}-rg"
 
 
@@ -13,7 +13,7 @@ locals {
           AZURE_CLIENT_ID       = data.azurerm_user_assigned_identity.cd_client_identity.client_id
           AZURE_SUBSCRIPTION_ID = data.azurerm_subscription.current.subscription_id
           AZURE_TENANT_ID       = data.azurerm_client_config.current.tenant_id
-          SLACK_WEBHOOK_URL = data.azurerm_key_vault_secret.slack_webhook.value
+          SLACK_WEBHOOK_URL     = data.azurerm_key_vault_secret.slack_webhook.value
         }
       ]
       repository_secrets   = []
@@ -26,7 +26,7 @@ locals {
           AZURE_CLIENT_ID       = data.azurerm_user_assigned_identity.cd_client_identity.client_id
           AZURE_SUBSCRIPTION_ID = data.azurerm_subscription.current.subscription_id
           AZURE_TENANT_ID       = data.azurerm_client_config.current.tenant_id
-          SLACK_WEBHOOK_URL = data.azurerm_key_vault_secret.slack_webhook.value
+          SLACK_WEBHOOK_URL     = data.azurerm_key_vault_secret.slack_webhook.value
         }
       ]
       repository_secrets   = []
