d56780c0b33283dc2b4753937b49dcc2a4c8cb76
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2025-07-31 10:48:10 +0000

    fix: Daily cap law & appinsights (#171)
    fix daily cap

--- src/70_domains/idpay_common/env/itn-prod/terraform.tfvars
+++ src/70_domains/idpay_common/env/itn-prod/terraform.tfvars
@@ -90,6 +90,5 @@ aks_nodepool_green = {
 #Monitoring
 law_sku               = "PerGB2018"
 law_retention_in_days = 30
-law_daily_quota_gb    = 10
 
 mcshared_dns_zone_prefix = "api-mcshared"
--- src/70_domains/mcshared-common/04_monitor.tf
+++ src/70_domains/mcshared-common/04_monitor.tf
@@ -17,11 +17,12 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
 
 ### 🔍 Application insights
 resource "azurerm_application_insights" "application_insights" {
-  name                = "${local.project}-appinsights"
-  location            = var.location
-  resource_group_name = local.monitor_rg_name
-  application_type    = "other"
-  workspace_id        = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  name                 = "${local.project}-appinsights"
+  location             = var.location
+  resource_group_name  = local.monitor_rg_name
+  application_type     = "other"
+  workspace_id         = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  daily_data_cap_in_gb = var.law_daily_quota_gb
 
   tags = module.tag_config.tags
 }
