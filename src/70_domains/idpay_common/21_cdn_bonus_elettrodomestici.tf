d7b0a6e91278f9d376ae977e84d8fc85b8d1a970
ffppa <fabio.felici@pagopa.it>
2025-07-31 14:42:23 +0000

    fix: Update Content-Security-Policy rules for enhanced security (#173)
    Update `Content-Security-Policy` rules for enhanced security and resource access configuration

--- src/70_domains/idpay_common/21_cdn_bonus_elettrodomestici.tf
+++ src/70_domains/idpay_common/21_cdn_bonus_elettrodomestici.tf
@@ -26,9 +26,19 @@ locals {
       value  = "max-age=31536000"
     },
     {
-      action = "Append"
+      action = "Overwrite"
       name   = contains(["d"], var.env_short) ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy"
       value  = "default-src 'self'; object-src 'none'; connect-src 'self' https://api-io.${var.dns_zone_prefix}.${var.external_domain}/ https://api-eu.mixpanel.com/track/; "
+    },
+    {
+      action = "Append"
+      name   = contains(["d"], var.env_short) ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy"
+      value  = "script-src 'self'; style-src 'self' 'unsafe-inline' https://selfcare${local.selfare_temp_suffix}.pagopa.it/assets/font/selfhostedfonts.css; worker-src 'none'; font-src 'self' https://selfcare${local.selfare_temp_suffix}.pagopa.it/assets/font/; "
+    },
+    {
+      action = "Append"
+      name   = contains(["d"], var.env_short) ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy"
+      value  = "img-src 'self' https://assets.cdn.io.italia.it https://${module.cdn_idpay_bonuselettrodomestici.storage_primary_web_host} https://${var.env != "prod" ? "${var.env}." : ""}selfcare${local.selfare_temp_suffix}.pagopa.it https://selc${var.env_short}checkoutsa.z6.web.core.windows.net/institutions/ data:; "
     },
     {
       action = "Append"
