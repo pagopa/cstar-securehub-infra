eded4cc1d2c16756ff3c9200bd0d8949fdbd35b4
antonio.tarricone <antonio.tarricone@lap-mbp16-n02-0659.station>
2025-08-05 15:27:35 +0200

    Ruleset splitted.

--- src/91_github/idpay/02_branch_protection.tf
+++ src/91_github/idpay/02_branch_protection.tf
@@ -6,10 +6,10 @@ resource "github_branch_default" "default" {
 }
 
 # iterate over the repositories and create branch protection rules for the develop branch
-resource "github_repository_ruleset" "pari" {
+resource "github_repository_ruleset" "develop" {
   for_each = local.repository
 
-  name        = "PARI"
+  name        = "develop"
   repository  = each.key
   target      = "branch"
   enforcement = "active"
@@ -17,9 +17,7 @@ resource "github_repository_ruleset" "pari" {
   conditions {
     ref_name {
       include = [
-        "refs/heads/develop",
-        "refs/heads/uat",
-        "refs/heads/main"
+        "refs/heads/develop"
       ]
       exclude = []
     }
@@ -27,8 +25,8 @@ resource "github_repository_ruleset" "pari" {
 
   bypass_actors {
     # repository admin
-    actor_id    = 5
-    actor_type  = "RepositoryRole"
+    actor_id    = 13205158
+    actor_type  = "Team"
     bypass_mode = "always"
   }
 
@@ -53,6 +51,74 @@ resource "github_repository_ruleset" "pari" {
       required_check {
         context        = "SonarCloud Code Analysis"
         integration_id = 12526
+      }
+    }
+
+    required_code_scanning {
+      required_code_scanning_tool {
+        tool                      = "CodeQL"
+        security_alerts_threshold = "high_or_higher"
+        alerts_threshold          = "errors"
+      }
+    }
+  }
+}
+
+# iterate over the repositories and create branch protection rules for the develop branch
+resource "github_repository_ruleset" "uat_and_main" {
+  for_each = local.repository
+
+  name        = "uat_and_main"
+  repository  = each.key
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = [
+        "refs/heads/uat",
+        "refs/heads/main"
+      ]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    # repository admin
+    actor_id    = 13205158
+    actor_type  = "Team"
+    bypass_mode = "always"
+  }
+
+  rules {
+    creation                = true
+    update                  = true
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+
+    pull_request {
+      require_code_owner_review         = true
+      required_approving_review_count   = 2
+      dismiss_stale_reviews_on_push     = true
+      required_review_thread_resolution = true
+      require_last_push_approval        = false
+    }
+
+    required_status_checks {
+      strict_required_status_checks_policy = true
+      do_not_enforce_on_create             = false
+      required_check {
+        context        = "SonarCloud Code Analysis"
+        integration_id = 12526
+      }
+    }
+
+    required_code_scanning {
+      required_code_scanning_tool {
+        tool                      = "CodeQL"
+        security_alerts_threshold = "high_or_higher"
+        alerts_threshold          = "errors"
       }
     }
   }
