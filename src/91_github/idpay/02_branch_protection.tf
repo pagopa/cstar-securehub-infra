a8e6321e5af4106306e63080376614309cd66695
antonio.tarricone <antonio.tarricone@lap-mbp16-n02-0659.station>
2025-08-05 10:47:30 +0200

    Updated ruleset definition.

--- src/91_github/idpay/02_branch_protection.tf
+++ src/91_github/idpay/02_branch_protection.tf
@@ -5,67 +5,55 @@ resource "github_branch_default" "default" {
   branch     = "main"
 }
 
-
 # iterate over the repositories and create branch protection rules for the develop branch
-resource "github_repository_ruleset" "dev" {
+resource "github_repository_ruleset" "pari" {
   for_each = local.repository
 
-  name        = "dev"
+  name        = "PARI"
   repository  = each.key
   target      = "branch"
   enforcement = "active"
 
   conditions {
     ref_name {
-      include = ["refs/heads/develop"]
+      include = [
+        "refs/heads/develop",
+        "refs/heads/uat",
+        "refs/heads/main"
+      ]
       exclude = []
     }
   }
 
+  bypass_actors {
+    # repository admin
+    actor_id    = 5
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+  }
+
   rules {
     creation                = true
-    update                  = false
+    update                  = true
     deletion                = true
-    required_linear_history = false
-    required_signatures     = false
+    non_fast_forward        = true
+    required_linear_history = true
 
     pull_request {
       require_code_owner_review         = true
       required_approving_review_count   = 2
       dismiss_stale_reviews_on_push     = true
       required_review_thread_resolution = true
+      require_last_push_approval        = false
     }
-  }
-}
 
-# iterate over the repositories and create branch protection rules for the develop branch
-resource "github_repository_ruleset" "uat_prod" {
-  for_each = local.repository
-
-  name        = "uat_prod"
-  repository  = each.key
-  target      = "branch"
-  enforcement = "active"
-
-  conditions {
-    ref_name {
-      include = ["refs/heads/uat", "refs/heads/main"]
-      exclude = []
-    }
-  }
-
-  rules {
-    creation                = true
-    update                  = false
-    deletion                = true
-    required_linear_history = false
-    required_signatures     = false
-
-    pull_request {
-      require_code_owner_review         = true
-      required_approving_review_count   = 2
-      dismiss_stale_reviews_on_push     = true
-      required_review_thread_resolution = true
+    required_status_checks {
+      strict_required_status_checks_policy = true
+      do_not_enforce_on_create             = false
+      required_check {
+        context        = "SonarCloud Code Analysis"
+        integration_id = 12526
+      }
     }
   }
 }
