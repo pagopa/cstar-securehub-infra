4028fdab22ec5c1f84926cda27d07f0930283b97
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-08-25 00:30:21 +0200

    created global rules for bonus

--- src/70_domains/idpay_common/21_cdn_bonus_elettrodomestici.tf
+++ src/70_domains/idpay_common/21_cdn_bonus_elettrodomestici.tf
@@ -19,12 +19,7 @@ locals {
 
   # Security Headers - Applied globally to all responses
   # These headers enhance security by preventing common attacks
-  security_headers = [
-    {
-      action = "Overwrite"
-      name   = "Strict-Transport-Security"
-      value  = "max-age=31536000"
-    },
+  security_headers_part1 = [
     {
       action = "Overwrite"
       name   = contains(["d"], var.env_short) ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy"
@@ -39,6 +34,14 @@ locals {
       action = "Append"
       name   = contains(["d"], var.env_short) ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy"
       value  = "img-src 'self' https://assets.cdn.io.italia.it https://${module.cdn_idpay_bonuselettrodomestici.storage_primary_web_host}; "
+    },
+  ]
+
+  security_headers_part2 = [
+    {
+      action = "Overwrite"
+      name   = "Strict-Transport-Security"
+      value  = "max-age=31536000"
     },
     {
       action = "Append"
@@ -59,7 +62,7 @@ locals {
     # Cittadino Application Rule - Handles citizen portal routing
     {
       name  = "CittadinoApplication"
-      order = 2
+      order = 10
       conditions = [
         {
           condition_type   = "url_path_condition"
@@ -92,7 +95,7 @@ locals {
     # Esercenti Application Rule - Handles merchant portal routing
     {
       name  = "PortaleEsercentiApplication"
-      order = 3
+      order = 11
       conditions = [
         {
           condition_type   = "url_path_condition"
@@ -132,7 +135,7 @@ locals {
   additional_delivery_rules = [
     {
       name  = "robotsNoIndex"
-      order = local.total_rewrite_rules + 2
+      order = 20 + local.total_rewrite_rules + 2
 
       // conditions
       url_path_conditions = [
@@ -173,7 +176,7 @@ locals {
     },
     {
       name  = "microcomponentsNoCache"
-      order = local.total_rewrite_rules + 3
+      order = 20 + local.total_rewrite_rules + 3
 
       // conditions
       url_path_conditions           = []
@@ -215,7 +218,7 @@ locals {
     },
     {
       name  = "RootRedirect"
-      order = local.total_rewrite_rules + 4
+      order = 20 + local.total_rewrite_rules + 4
 
       // conditions
       url_path_conditions = [
@@ -262,138 +265,129 @@ locals {
 
 // Public CDN to serve frontend - main domain
 module "cdn_idpay_bonuselettrodomestici" {
-  source = "./.terraform/modules/__v4__/cdn"
+  # source = "./.terraform/modules/__v4__/cdn"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//cdn_frontdoor?ref=PAYMCLOUD-477-v-4-creazione-modulo-cdn-front-door-per-sostituire-cdn-classic-deprecata"
 
   # Basic Configuration
-  name                = "bonus"
-  prefix              = local.project_weu
+  cdn_prefix_name     = "${local.project}-bonus"
   resource_group_name = data.azurerm_resource_group.idpay_data_rg.name
   location            = var.location
-  cdn_location        = var.location_weu
 
-  # DNS Configuration
-  hostname = local.bonus_dns_zone.name
-
-  # Enforce to not use module null_resource, the dns_zone_name must be different by hostname.
-  ## - This was made for skip single custom domain setup for custom multi-domain setup.
-  ##  * Includes: .it, .com, .info, .io, .net, .eu
-  dns_zone_name                = "dummy"
-  dns_zone_resource_group_name = "dummy"
-  create_dns_record            = false # Managed manually below
+  enable_custom_domain = false
 
   # Storage Configuration
-  storage_account_name             = local.cdn_storage_account_name
-  storage_account_replication_type = var.idpay_cdn_storage_account_replication_type
-  index_document                   = local.cdn_index_document
-  error_404_document               = local.cdn_error_document
+  storage_account_name               = local.cdn_storage_account_name
+  storage_account_replication_type   = var.idpay_cdn_storage_account_replication_type
+  storage_account_index_document     = local.cdn_index_document
+  storage_account_error_404_document = local.cdn_error_document
 
   # Security Configuration
-  https_rewrite_enabled              = true
-  advanced_threat_protection_enabled = var.idpay_cdn_sa_advanced_threat_protection_enabled
+  https_rewrite_enabled = true
 
   # Key Vault Configuration
-  keyvault_resource_group_name = local.idpay_kv_rg_name
-  keyvault_subscription_id     = data.azurerm_subscription.current.subscription_id
-  keyvault_vault_name          = local.idpay_kv_name
+  keyvault_id = data.azurerm_key_vault.domain_kv.id
 
   # Caching Configuration
-  querystring_caching_behaviour = "BypassCaching"
+  querystring_caching_behaviour = "IgnoreQueryString"
   log_analytics_workspace_id    = data.azurerm_log_analytics_workspace.core_log_analytics.id
 
   # Global Delivery Rules
-  global_delivery_rule = {
-    cache_expiration_action       = []
-    cache_key_query_string_action = []
-    modify_request_header_action  = []
-    modify_response_header_action = local.security_headers
-  }
+  global_delivery_rules = [
+    {
+      order                         = 1
+      modify_response_header_action = local.security_headers_part1
+    },
+    {
+      order                         = 2
+      modify_response_header_action = local.security_headers_part2
+  }]
 
   # Application-specific Delivery Rules (rewrite only)
   delivery_rule_rewrite = local.app_delivery_rules
 
-  # Generic Delivery Rules (including redirects)
-  delivery_rule = local.additional_delivery_rules
+  # # Generic Delivery Rules (including redirects)
+  # delivery_rule = local.additional_delivery_rules
 
   tags = module.tag_config.tags
 }
 
 
-/**
- * DNS Records for all bonus elettrodomestici apex zones
- * Includes: .it, .com, .info, .io, .net, .eu, pagopa.it
- */
-
-# A Record for APEX domain for ALL bonus elettrodomestici zones
-resource "azurerm_dns_a_record" "bonus_all_zones_apex" {
-  for_each = local.all_bonus_zones_apex
-
-  name                = "@"
-  zone_name           = each.value.name
-  resource_group_name = each.value.resource_group_name
-  ttl                 = 300
-  target_resource_id  = module.cdn_idpay_bonuselettrodomestici.id
-
-  tags = module.tag_config.tags
-}
-
-# CNAME cdnverify record for ALL bonus elettrodomestici zones
-resource "azurerm_dns_cname_record" "bonus_all_zones_cdnverify" {
-  for_each = local.all_bonus_zones_apex
-
-  name                = "cdnverify"
-  zone_name           = each.value.name
-  resource_group_name = each.value.resource_group_name
-  ttl                 = 300
-  record              = "cdnverify.${module.cdn_idpay_bonuselettrodomestici.hostname}"
-
-  tags = module.tag_config.tags
-}
-
-/**
- * Custom Domain Configuration for Azure CDN (Classic)
- * Associates all bonus elettrodomestici domains to the CDN endpoint
- * Note: Using azurerm_cdn_endpoint_custom_domain for Azure CDN Classic
- */
-
-# Custom Domain for each bonus elettrodomestici zone apex - Azure CDN Classic
-resource "azurerm_cdn_endpoint_custom_domain" "bonus_custom_domains" {
-  for_each = local.all_bonus_zones_apex
-
-  name            = replace(replace(each.key, ".", "-"), "_", "-")
-  cdn_endpoint_id = module.cdn_idpay_bonuselettrodomestici.id
-  host_name       = each.value.name
-
-  # Enable HTTPS with CDN user defined certificate
-  user_managed_https {
-    key_vault_secret_id = data.azurerm_key_vault_certificate.bonus_elettrodomestici_cert_apex[
-      join("-", split(".", each.key))
-    ].versionless_secret_id
-    tls_version = "TLS12"
-  }
-
-  # Depends on DNS records for domain verification
-  depends_on = [
-    azurerm_dns_a_record.bonus_all_zones_apex,
-    azurerm_dns_cname_record.bonus_all_zones_cdnverify
-  ]
-}
-
-data "azuread_service_principal" "microsoft_cdn" {
-  display_name = "Microsoft.AzureFrontDoor-Cdn"
-}
-
-resource "azurerm_key_vault_access_policy" "cdn_certificates_policy" {
-  key_vault_id = data.azurerm_key_vault.domain_kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azuread_service_principal.microsoft_cdn.object_id
-
-  certificate_permissions = [
-    "Get",
-    "List"
-  ]
-
-  secret_permissions = [
-    "Get",
-    "List"
-  ]
-}
+# /**
+#  * DNS Records for all bonus elettrodomestici apex zones
+#  * Includes: .it, .com, .info, .io, .net, .eu, pagopa.it
+#  */
+#
+# # A Record for APEX domain for ALL bonus elettrodomestici zones
+# resource "azurerm_dns_a_record" "bonus_all_zones_apex" {
+#   for_each = local.all_bonus_zones_apex
+#
+#   name                = "@"
+#   zone_name           = each.value.name
+#   resource_group_name = each.value.resource_group_name
+#   ttl                 = 300
+#   target_resource_id  = module.cdn_idpay_bonuselettrodomestici.id
+#
+#   tags = module.tag_config.tags
+# }
+#
+# # CNAME cdnverify record for ALL bonus elettrodomestici zones
+# resource "azurerm_dns_cname_record" "bonus_all_zones_cdnverify" {
+#   for_each = local.all_bonus_zones_apex
+#
+#   name                = "cdnverify"
+#   zone_name           = each.value.name
+#   resource_group_name = each.value.resource_group_name
+#   ttl                 = 300
+#   record              = "cdnverify.${module.cdn_idpay_bonuselettrodomestici.hostname}"
+#
+#   tags = module.tag_config.tags
+# }
+#
+# /**
+#  * Custom Domain Configuration for Azure CDN (Classic)
+#  * Associates all bonus elettrodomestici domains to the CDN endpoint
+#  * Note: Using azurerm_cdn_endpoint_custom_domain for Azure CDN Classic
+#  */
+#
+# # Custom Domain for each bonus elettrodomestici zone apex - Azure CDN Classic
+# resource "azurerm_cdn_endpoint_custom_domain" "bonus_custom_domains" {
+#   for_each = local.all_bonus_zones_apex
+#
+#   name            = replace(replace(each.key, ".", "-"), "_", "-")
+#   cdn_endpoint_id = module.cdn_idpay_bonuselettrodomestici.id
+#   host_name       = each.value.name
+#
+#   # Enable HTTPS with CDN user defined certificate
+#   user_managed_https {
+#     key_vault_secret_id = data.azurerm_key_vault_certificate.bonus_elettrodomestici_cert_apex[
+#       join("-", split(".", each.key))
+#     ].versionless_secret_id
+#     tls_version = "TLS12"
+#   }
+#
+#   # Depends on DNS records for domain verification
+#   depends_on = [
+#     azurerm_dns_a_record.bonus_all_zones_apex,
+#     azurerm_dns_cname_record.bonus_all_zones_cdnverify
+#   ]
+# }
+#
+# data "azuread_service_principal" "microsoft_cdn" {
+#   display_name = "Microsoft.AzureFrontDoor-Cdn"
+# }
+#
+# resource "azurerm_key_vault_access_policy" "cdn_certificates_policy" {
+#   key_vault_id = data.azurerm_key_vault.domain_kv.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azuread_service_principal.microsoft_cdn.object_id
+#
+#   certificate_permissions = [
+#     "Get",
+#     "List"
+#   ]
+#
+#   secret_permissions = [
+#     "Get",
+#     "List"
+#   ]
+# }
