72adc3008dd92b84aeba8921d732758e0b24e903
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-08-27 11:56:21 +0200

    cdn selfcare uodated

--- src/70_domains/idpay_common/21_cdn_selfcare.tf
+++ src/70_domains/idpay_common/21_cdn_selfcare.tf
@@ -1,58 +1,30 @@
 module "cdn_idpay_selfcare" {
-  source = "./.terraform/modules/__v4__/cdn"
+  # source = "./.terraform/modules/__v4__/cdn"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//cdn_frontdoor?ref=PAYMCLOUD-477-v-4-creazione-modulo-cdn-front-door-per-sostituire-cdn-classic-deprecata"
 
-  name                = "selfcare"
-  prefix              = local.project_weu
+  cdn_prefix_name     = "${local.project}-selfcare"
   resource_group_name = data.azurerm_resource_group.idpay_data_rg.name
   location            = var.location
-  cdn_location        = var.location_weu
 
-  hostname              = "selfcare-italy.${data.azurerm_dns_zone.public_cstar.name}"
-  https_rewrite_enabled = true
-
-  storage_account_name             = "${local.project}selcdnsa"
-  storage_account_replication_type = var.selfcare_welfare_cdn_storage_account_replication_type
-  index_document                   = "index.html"
-  error_404_document               = "not_found.html"
-
-  dns_zone_name                = data.azurerm_dns_zone.public_cstar.name
-  dns_zone_resource_group_name = data.azurerm_dns_zone.public_cstar.resource_group_name
-
-  keyvault_resource_group_name = local.idpay_kv_rg_name
-  keyvault_subscription_id     = data.azurerm_subscription.current.subscription_id
-  keyvault_vault_name          = local.idpay_kv_name
-
-  querystring_caching_behaviour      = "BypassCaching"
-  advanced_threat_protection_enabled = var.idpay_cdn_sa_advanced_threat_protection_enabled
-
-  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.core_log_analytics.id
-
-
-  // https://antbutcher.medium.com/hosting-a-react-js-app-on-azure-blob-storage-azure-cdn-for-ssl-and-routing-8fdf4a48feeb
-  // it is important to add base tag in index.html too (i.e. <base href="/">)
-  delivery_rule_rewrite = [{
-    name  = "RewriteRules"
-    order = 2
-
-    conditions = [{
-      condition_type   = "url_file_extension_condition"
-      operator         = "LessThan"
-      match_values     = ["1"]
-      transforms       = []
-      negate_condition = false
-    }]
-
-    url_rewrite_action = {
-      source_pattern          = "/"
-      destination             = "/index.html"
-      preserve_unmatched_path = false
+  custom_domains = [
+    {
+      domain_name             = "selfcare-italy.${data.azurerm_dns_zone.public_cstar.name}"
+      dns_name                = data.azurerm_dns_zone.public_cstar.name
+      dns_resource_group_name = data.azurerm_dns_zone.public_cstar.resource_group_name
+      ttl                     = var.env != "p" ? 300 : 3600
     }
-  }]
+  ]
 
-  global_delivery_rule = {
-    cache_expiration_action       = []
-    cache_key_query_string_action = []
-    modify_request_header_action  = []
+  storage_account_name               = "${local.project}selcdnsa"
+  storage_account_replication_type   = var.selfcare_welfare_cdn_storage_account_replication_type
+  storage_account_index_document     = "index.html"
+  storage_account_error_404_document = "not_found.html"
+
+  querystring_caching_behaviour = "IgnoreQueryString"
+  log_analytics_workspace_id    = data.azurerm_log_analytics_workspace.core_log_analytics.id
+
+  global_delivery_rules = [{
+    order = 1
 
     # HSTS
     modify_response_header_action = [{
@@ -61,7 +33,26 @@ module "cdn_idpay_selfcare" {
       value  = "max-age=31536000"
       },
     ]
-  }
+  }]
+
+  // https://antbutcher.medium.com/hosting-a-react-js-app-on-azure-blob-storage-azure-cdn-for-ssl-and-routing-8fdf4a48feeb
+  // it is important to add base tag in index.html too (i.e. <base href="/">)
+  delivery_rule_rewrite = [{
+    name  = "RewriteRules"
+    order = 2
+
+    url_file_extension_conditions = [{
+      operator         = "LessThan"
+      match_values     = ["1"]
+      transforms       = []
+      negate_condition = false
+    }]
+    url_rewrite_actions = [{
+      source_pattern          = "/"
+      destination             = "/index.html"
+      preserve_unmatched_path = false
+    }]
+  }]
 
   tags = module.tag_config.tags
 }
