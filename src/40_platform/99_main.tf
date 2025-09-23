17089284b8125ef2d4a94b36076e06af95b85dd7
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-09-23 10:55:26 +0200

    minor fix

--- src/40_platform/99_main.tf
+++ src/40_platform/99_main.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.0"
