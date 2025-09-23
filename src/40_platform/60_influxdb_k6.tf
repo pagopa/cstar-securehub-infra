f1238ce715dbd9dffb01a70d98459bd7c490327b
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-09-23 12:38:08 +0200

    - fix password and token - added cert mounter

--- src/40_platform/60_influxdb_k6.tf
+++ src/40_platform/60_influxdb_k6.tf
@@ -12,13 +12,13 @@ locals {
 # password and token generation
 #-------------------------------------------------------------------------
 resource "random_password" "influxdb_admin_password" {
-  length  = 24
-  special = true
+  length           = 33
+  special          = false
 }
 
 resource "random_password" "influxdb_admin_token" {
-  length  = 40
-  special = false
+  length           = 33
+  special          = false
 }
 
 resource "azurerm_key_vault_secret" "influxdb_admin_username" {
@@ -80,8 +80,8 @@ resource "argocd_application" "influxdb2" {
         values = templatefile("${path.module}/aks/influxdb/values.yaml", {
           repository  = var.influxdb2_helm.image.name
           tag         = var.influxdb2_helm.image.tag
-          hostname    = local.influxdb_url
-          tls_secret_name = replace(local.influxdb_url, ".", "-")
+          hostname    = local.influxdb_internal_url
+          tls_secret_name = replace(local.influxdb_internal_url, ".", "-")
           tolerations = try(var.influxdb2_helm.tolerations, [])
           affinity    = try(var.influxdb2_helm.affinity, {})
           admin_user = {
@@ -112,3 +112,70 @@ resource "azurerm_private_dns_a_record" "influxdb_ingress" {
 
   tags = module.tag_config.tags
 }
+
+#---------------------------------------------------------------
+# tools
+#---------------------------------------------------------------
+
+module "influxdb_cert_mounter_workload_identity_init" {
+  source = "./.terraform/modules/__v4__/kubernetes_workload_identity_init"
+
+  workload_identity_name_prefix         = "influxdb-cert-mounter"
+  workload_identity_resource_group_name = data.azurerm_kubernetes_cluster.aks.resource_group_name
+  workload_identity_location            = var.location
+
+  depends_on = [
+  argocd_application.influxdb2
+  ]
+}
+
+module "influxdb_cert_mounter_workload_identity_configuration" {
+  source = "./.terraform/modules/__v4__/kubernetes_workload_identity_configuration"
+
+  workload_identity_name_prefix         = "influxdb-cert-mounter"
+  workload_identity_resource_group_name = data.azurerm_kubernetes_cluster.aks.resource_group_name
+  aks_name                              = data.azurerm_kubernetes_cluster.aks.name
+  aks_resource_group_name               = data.azurerm_kubernetes_cluster.aks.resource_group_name
+  namespace                             = local.influxdb_namespace
+
+  key_vault_id                      = data.azurerm_key_vault.core_kv.id
+  key_vault_certificate_permissions = ["Get"]
+  key_vault_key_permissions         = ["Get"]
+  key_vault_secret_permissions      = ["Get"]
+
+  depends_on = [
+    argocd_application.influxdb2,
+    module.influxdb_cert_mounter_workload_identity_init
+  ]
+
+}
+
+module "influxdb_cert_mounter_internal_domain_certificate" {
+  source           = "./.terraform/modules/__v4__/cert_mounter"
+  namespace        = local.influxdb_namespace
+  certificate_name = replace(local.influxdb_internal_url, ".", "-")
+  kv_name          = data.azurerm_key_vault.core_kv.name
+  tenant_id        = data.azurerm_subscription.current.tenant_id
+
+  workload_identity_service_account_name = module.influxdb_cert_mounter_workload_identity_configuration.workload_identity_service_account_name
+  workload_identity_client_id            = module.influxdb_cert_mounter_workload_identity_configuration.workload_identity_client_id
+
+  depends_on = [
+    argocd_application.influxdb2,
+    module.influxdb_cert_mounter_workload_identity_configuration
+  ]
+}
+
+resource "helm_release" "reloader_influxdb" {
+  name       = "reloader"
+  repository = "https://stakater.github.io/stakater-charts"
+  chart      = "reloader"
+  version    = "v1.0.30"
+  namespace  = local.influxdb_namespace
+
+  set {
+    name  = "reloader.watchGlobally"
+    value = "false"
+  }
+}
+
