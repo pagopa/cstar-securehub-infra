cb460593372249e672777aa0f3934168c844120e
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-09-26 12:30:24 +0200

    SRTP-702-create-srtp-workbook

--- src/70_domains/srtp_common/13_workbooks.tf
+++ src/70_domains/srtp_common/13_workbooks.tf
@@ -2,7 +2,7 @@ resource "azurerm_application_insights_workbook" "srtp_workbook" {
   name                = uuidv5("oid", "srtp-workbook")
   resource_group_name = data.azurerm_resource_group.srtp_monitoring_rg.name
   location            = data.azurerm_resource_group.srtp_monitoring_rg.location
-  display_name        = "SRTP Workbook"
+  display_name        = "SRTP OER"
 
   data_json = templatefile("${path.module}/workbooks/SRTPWorkbook.json.tpl", {
     subscription_id = data.azurerm_subscription.current.subscription_id
