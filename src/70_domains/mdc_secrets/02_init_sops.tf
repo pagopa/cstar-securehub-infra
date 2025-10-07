4df64764a9109ad8730016e998f97a571a3f23cf
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-10-07 17:40:24 +0200

    setup dei kv in italy

--- src/70_domains/mdc_secrets/02_init_sops.tf
+++ src/70_domains/mdc_secrets/02_init_sops.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_key" "sops" {
-  name         = "${local.product}-${var.domain}-sops-key"
-  key_vault_id = module.key_vault["core"].id
+  name         = "${local.project}-sops-key"
+  key_vault_id = module.key_vault[var.domain].id
   key_type     = "RSA"
   key_size     = 2048
 
