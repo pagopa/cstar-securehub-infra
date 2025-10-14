135e305ff0a8711aefb005eb8ce35b7df23f34df
dario.pelliccioli <dario.pelliccioli@nttdata.com>
2025-10-14 13:23:30 +0200

    add cronjob for expiring voucher (and reminder)

--- src/70_domains/idpay_app/09_k8s_cronjobs.tf
+++ src/70_domains/idpay_app/09_k8s_cronjobs.tf
@@ -9,6 +9,7 @@ resource "kubernetes_cron_job_v1" "cancel_pending_transactions" {
 
   spec {
     schedule           = "0 23 * * *" #  # During standard time (Oct–Mar) runs at 00:00 Italy time, During daylight saving time (Mar–Oct) runs at 01:00 Italy time
+    timezone           = "Europe/Rome"
     concurrency_policy = "Forbid"
 
     job_template {
@@ -32,6 +33,97 @@ resource "kubernetes_cron_job_v1" "cancel_pending_transactions" {
               args = [
                 "-X", "DELETE",
                 "https://${local.idpay_ingress_url}/idpaypayment/idpay/payment/pendingTransactions"
+              ]
+            }
+            restart_policy = "OnFailure"
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_cron_job_v1" "cancel_expired_vouchers" {
+  metadata {
+    name      = "cancel-expired-vouchers"
+    namespace = var.domain
+    labels = {
+      app = "idpay-app"
+    }
+  }
+
+  spec {
+    schedule           = "5 0 * * *" #00:05
+    timezone           = "Europe/Rome"
+    concurrency_policy = "Forbid"
+
+    job_template {
+      metadata {
+        name = "cancel-expired-vouchers-job"
+        labels = {
+          app = "idpay-app"
+        }
+      }
+      spec {
+        template {
+          metadata {
+            labels = {
+              app = "idpay-app"
+            }
+          }
+          spec {
+            container {
+              name  = "cancel-expired-vouchers"
+              image = "curlimages/curl:8.1.2"
+              args = [
+                "-X", "POST",
+                "https://${local.idpay_ingress_url}/idpaypayment/idpay/transactions/expired/initiatives/${var.idpay_bel_initiative_id}/update-status"
+              ]
+            }
+            restart_policy = "OnFailure"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_cron_job_v1" "reminder_voucher_expiration" {
+  metadata {
+    name      = "reminder-voucher-expiration"
+    namespace = var.domain
+    labels = {
+      app = "idpay-app"
+    }
+  }
+
+  spec {
+    schedule           = "0 1 * * *" # 01:00
+    timezone           = "Europe/Rome"
+    concurrency_policy = "Forbid"
+
+    job_template {
+      metadata {
+        name = "reminder-voucher-expiration-job"
+        labels = {
+          app = "idpay-app"
+        }
+      }
+      spec {
+        template {
+          metadata {
+            labels = {
+              app = "idpay-app"
+            }
+          }
+          spec {
+            container {
+              name  = "reminder-voucher-expiration"
+              image = "curlimages/curl:8.1.2"
+              args = [
+                "-X", "PUT",
+                "https://${local.idpay_ingress_url}/idpaywallet/idpay/wallet/batch/run/${var.idpay_bel_initiative_id}"
               ]
             }
             restart_policy = "OnFailure"
