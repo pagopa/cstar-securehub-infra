fe89a3392647f8b96ca75869d26f32f866914fc9
stefano.delia <stefano.delia@pagopa.it>
2025-10-28 16:35:23 +0100

    fix: add storage idpay exports

---
+++ src/70_domains/idpay_common/08_storage_export.tf
@@ -0,0 +1,23 @@
+module "storage_idpay_exports" {
+  source              = "./.terraform/modules/__v4__/IDH/storage_account"
+  product_name        = var.prefix
+  env                 = var.env
+  location            = var.location
+  resource_group_name = local.data_rg
+  tags                = module.tag_config.tags
+
+  idh_resource_tier   = var.env_short != "prod" ? "basic" : "standard"
+
+  name   = replace("${local.project}-exports-sa", "-", "")
+  domain = var.domain
+
+  # Network
+  private_dns_zone_blob_ids  = [data.azurerm_private_dns_zone.blob_storage.id]
+  private_endpoint_subnet_id = module.private_endpoint_storage_snet.id
+}
+
+resource "azurerm_storage_container" "idpay_export_products_container" {
+  name                  = "export"
+  storage_account_id    = module.storage_idpay_exports.id
+  container_access_type = "private"
+}
