b3b96b9987f9e32a9224df4330facfb7d3590cc8
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2025-11-10 12:09:04 +0100

    fix Inconsistent conditional result types

--- src/70_domains/idpay_app/12_alerts_locals.tf
+++ src/70_domains/idpay_app/12_alerts_locals.tf
@@ -1502,11 +1502,9 @@ locals {
     }
   }
 
-  alert_definitions = merge([
+  alert_definitions = contains(["u", "p"], var.env_short) ? merge([
     for service in values(local.alerts_microservices) : merge([
       for group in values(service) : group
     ]...)
-  ]...)
-
-  empty_alerts_map = { for k, v in local.alert_definitions : k => null }
+  ]...) : {}
 }
