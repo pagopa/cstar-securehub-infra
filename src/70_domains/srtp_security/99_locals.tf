7a7751c7f202c3ea9027c4538a60161ad4129c23
Fabio Felici <fabio.felici@pagopa.it>
2025-11-13 11:42:23 +0100

    - Update `99_locals.tf` for consistent naming conventions and add managed identity naming. - Add `02_azdo_config.tf` to define AzDO managed identity Key Vault access policy. - Introduce AzDO managed identity data resource in `00_data.tf`.

--- src/70_domains/srtp_security/99_locals.tf
+++ src/70_domains/srtp_security/99_locals.tf
@@ -3,11 +3,11 @@ locals {
   project_nodomain = "${var.prefix}-${var.env_short}-${var.location_short}"               // e.g. "cstar-dev-ny"
   product          = "${var.prefix}-${var.env_short}"                                     // e.g. "cstar-dev"
 
-  azdo_managed_identity_rg_name = "${local.project_nodomain}-core-identity-rg"                                  // e.g. "cstar-dev-ny-core-identity-rg"
-  azdo_iac_managed_identities   = toset(["azdo-${var.env}-cstar-iac-deploy", "azdo-${var.env}-cstar-iac-plan"]) // e.g. ["azdo-dev-cstar-iac-deploy", "azdo-dev-cstar-iac-plan"]
-
+  azdo_managed_identity_rg_name = "${local.product}-identity-rg"
+  azdo_managed_identity_name    = upper("${var.env}-${var.prefix}")
+  # TODO create dedicated uid
   # this are the folder names inside the secrets folder in idpay_security
   secrets_folders_kv = ["srtp"] // e.g. ["core", "cicd"]
-  input_file         = "./secrets/${var.domain}/${var.location_short}-${var.env}"
 
+  input_file = "./secrets/${var.domain}/${var.location_short}-${var.env}"
 }
