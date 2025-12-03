ecbd3aba90c945a848d7ff450399981bbb4290a5
Christian Geovese <cgenoves@emeal.nttdata.com>
2025-12-03 15:44:08 +0100

    add index to transaction

--- src/70_domains/idpay_common/03_cosmos_mongodb_idpays_shared.tf
+++ src/70_domains/idpay_common/03_cosmos_mongodb_idpays_shared.tf
@@ -385,6 +385,7 @@ locals {
         { keys = ["status"], unique = false },
         { keys = ["additionalProperties.productName"], unique = false },
         { keys = ["rewardBatchId", "samplingKey"], unique = false },
+        { keys = ["rewardBatchTrxStatus"], unique = false },
       ]
     },
     {
