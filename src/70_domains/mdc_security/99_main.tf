0e32515810e390632baf1a0adf77040aeba83e73
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2026-02-18 16:15:02 +0100

    fix branch

--- src/70_domains/mdc_security/99_main.tf
+++ src/70_domains/mdc_security/99_main.tf
@@ -32,6 +32,6 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 module "__v4__" {
-  # https://github.com/pagopa/terraform-azurerm-v4/releases/tag/v7.56.0
-  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git?ref=fix-access-policy"
+  # https://github.com/pagopa/terraform-azurerm-v4/releases/tag/v9.1.1
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git?ref=9017adf2e89d80d014b3787eb95974835b31f08e"
 }
