ba8a49fb745ba963290d494727bbc74edf5d9770
marco.meloni <marco.meloni@emeal.nttdata.com>
2026-03-17 15:21:33 +0100

    feat: add resource of alerts and actiong group for MDC

--- src/70_domains/mdc_app/99_locals.tf
+++ src/70_domains/mdc_app/99_locals.tf
@@ -35,4 +35,42 @@ locals {
 
   ### ARGOCD
   argocd_internal_url = "argocd.${var.location_short}.${var.dns_zone_internal_prefix}.${var.external_domain}"
+
+  # 🔔 Alerts
+  # Base configuration shared across all alerts.
+  # Modify here to change global behavior.
+  base_alert_config = {
+    evaluation_frequency         = "PT10M"
+    window_duration              = "PT10M"
+    severity                     = 1
+    threshold                    = 0
+    enabled                      = contains(["p"], var.env_short) ? true : false
+    operator                     = "GreaterThan"
+    time_aggregation_method      = "Count"
+    minimum_failing_periods      = 1
+    number_of_evaluation_periods = 1
+    email_address                = "team_sw_client_msgcor-aaaanhfcws5qn5ndc4a4cjzm7m@pagopaspa.slack.com"
+  }
+
+  # MDC domain alerts map.
+  # To add a new alert: add an entry here + the corresponding .kql file
+  alerts_mdc = {
+    retry_failures = {
+      name          = "${local.project}-retry-failures-${var.env_short}"
+      description   = "Alert: Send of message failed after 3 retry. Environment: ${var.env}"
+      query         = file("${path.module}/queries-KQL/retry_failures.kql")
+    }
+    api_5xx_errors = {
+      name          = "${local.project}-5xx-errors-${var.env_short}"
+      description   = "Alert: Error 5XX on SEND API. Environment: ${var.env}"
+      query         = file("${path.module}/queries-KQL/api_5xx_errors.kql")
+    }
+  }
+
+
+  # Merge base config + alert-specific data.
+  final_alerts = {
+    for key, alert in local.alerts_mdc :
+    key => merge(local.base_alert_config, alert)
+  }
 }
