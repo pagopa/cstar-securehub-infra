466b867e6026e277c722d041ffe70cb1d36b0367
marco.meloni <marco.meloni@emeal.nttdata.com>
2026-03-18 11:07:55 +0100

    chore: add subject on email of alert on mdc

--- src/70_domains/mdc_app/12_alerts.tf
+++ src/70_domains/mdc_app/12_alerts.tf
@@ -44,6 +44,9 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "alerts" {
 
   action {
     action_groups = [azurerm_monitor_action_group.alerts[each.key].id]
+    custom_properties = {
+      "Email.Subject" = each.value.description
+    }
   }
 
   tags = local.tags
