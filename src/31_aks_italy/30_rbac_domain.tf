ce16a18de0fa949816ea13be53f8a445acc8e215
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2026-03-25 10:56:01 +0100

    wip

--- src/31_aks_italy/30_rbac_domain.tf
+++ src/31_aks_italy/30_rbac_domain.tf
@@ -16,3 +16,53 @@ resource "kubernetes_role_binding" "ad_group_binding" {
     api_group = "rbac.authorization.k8s.io"
   }
 }
+
+resource "kubernetes_role_binding" "ad_group_idpay_binding" {
+  metadata {
+    name      = "ad-group-idpay-edit-binding"
+    namespace = "idpay"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "edit"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "3715c6e7-28e1-4e9d-bbf1-67976dd0b6d9" # AD
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_cluster_role" "ad_group_cluster_role_binding_reader" {
+  metadata {
+    name = "ad-group-cluster-role"
+  }
+
+  rule {
+    api_groups = ["rbac.authorization.k8s.io"]
+    resources  = ["rolebindings", "roles"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_role_binding" "ad_group_idpay_binding_v2" {
+  metadata {
+    name      = "ad-group-idpay-view-binding"
+    namespace = "idpay"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.ad_group_cluster_role_binding_reader.metadata[0].name
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "3715c6e7-28e1-4e9d-bbf1-67976dd0b6d9" # AD
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
