c1b3be44570ec988b0367c131a0e38e4bea27ab4
fabio.picano <fabio.picano@nttdata.com>
2026-03-26 16:11:21 +0100

    Add keycloak on mdc terraform

---
+++ src/70_domains/mdc_common/60_keycloak_realm_mdc.tf
@@ -0,0 +1,17 @@
+# Realm configuration for Keycloak
+resource "keycloak_realm" "mdc" {
+  realm             = "mdc"
+  enabled           = true
+  display_name      = "Messaggi di cortesia"
+
+  internationalization {
+    supported_locales = [
+      "it"
+    ]
+    default_locale = "it"
+  }
+
+  # Durata dei token (opzionale)
+  access_token_lifespan = "30m" 
+    
+}
