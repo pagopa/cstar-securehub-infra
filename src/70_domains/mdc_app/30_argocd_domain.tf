25add72e6dca3f7826d45a8fc7fcef71aea2c3af
clonder0 <christian.londero@pagopa.it>
2026-03-26 17:12:17 +0100

    fix: add new ar backoffice mdc apps

--- src/70_domains/mdc_app/30_argocd_domain.tf
+++ src/70_domains/mdc_app/30_argocd_domain.tf
@@ -25,6 +25,16 @@ locals {
         name          = "emd-tpp"
         target_branch = "main"
         env           = ["dev", "uat", "prod"]
+      },
+      "emd-ar-backoffice-fe" = {
+        name          = "emd-ar-backoffice-fe"
+        target_branch = "main"
+        env           = ["dev", "uat", "prod"]
+      },
+      "emd-ar-backoffice-bff" = {
+        name          = "emd-ar-backoffice-bff"
+        target_branch = "main"
+        env           = ["dev", "uat", "prod"]
       }
     }
   }
