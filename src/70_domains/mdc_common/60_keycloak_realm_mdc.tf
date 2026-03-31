3c5c8225dd499582ef86078311489e2c5d7cf99b
fabio.picano <fabio.picano@nttdata.com>
2026-03-31 15:23:56 +0200

    Update token claims

--- src/70_domains/mdc_common/60_keycloak_realm_mdc.tf
+++ src/70_domains/mdc_common/60_keycloak_realm_mdc.tf
@@ -12,7 +12,45 @@ resource "keycloak_realm" "mdc" {
 
 }
 
-# create send client
+### Client settings ###
+
+# Create the scope for groups
+resource "keycloak_openid_client_scope" "mdc_base_claims" {
+  realm_id = keycloak_realm.mdc.id
+  name     = "mdc-claims"
+}
+
+# Add the mapper to the scope to include group information in the token
+resource "keycloak_openid_group_membership_protocol_mapper" "groups_mapper" {
+  realm_id            = keycloak_realm.mdc.id
+  client_scope_id     = keycloak_openid_client_scope.mdc_base_claims.id
+  name                = "groups-mapper"
+  claim_name          = "groups"
+  full_path           = false
+  add_to_access_token = true
+  add_to_id_token     = true
+}
+
+# Add audience to the token
+resource "keycloak_openid_audience_protocol_mapper" "aud_mapper" {
+  realm_id        = keycloak_realm.mdc.id
+  client_scope_id = keycloak_openid_client_scope.mdc_base_claims.id
+  name            = "audience-mapper"
+  included_custom_audience = "keycloak.pagopa.it" # Valore dell'audience per tutti
+  add_to_access_token      = true
+}
+
+# Mapper for clientId - The User Session Note Mapper extract the current client_id
+resource "keycloak_openid_user_session_note_protocol_mapper" "client_id_mapper" {
+  realm_id        = keycloak_realm.mdc.id
+  client_scope_id = keycloak_openid_client_scope.mdc_base_claims.id
+  name            = "client-id-mapper"
+  claim_name      = "clientId"
+  session_note    = "client_id"
+  add_to_access_token = true
+}
+
+# Create send client
 resource "keycloak_openid_client" "emd_pagopa_mdc_send_client" {
   realm_id = keycloak_realm.mdc.id
   name     = "send client"
@@ -24,6 +62,18 @@ resource "keycloak_openid_client" "emd_pagopa_mdc_send_client" {
   access_type              = "CONFIDENTIAL"
   service_accounts_enabled = true
 
+}
+
+# Send client default scopes
+resource "keycloak_openid_client_default_scopes" "emd_pagopa_mdc_send_client_default_scopes" {
+  realm_id  = keycloak_realm.mdc.id
+  client_id = keycloak_openid_client.emd_pagopa_mdc_send_client.id
+
+  default_scopes = [
+    "openid",
+    "profile",
+    keycloak_openid_client_scope.mdc_base_claims.name,
+  ]
 }
 
 # create pagopa client
@@ -40,6 +90,18 @@ resource "keycloak_openid_client" "emd_pagopa_mdc_pagopa_client" {
 
 }
 
+# Pagopa client default scopes
+resource "keycloak_openid_client_default_scopes" "emd_pagopa_mdc_pagopa_client_default_scopes" {
+  realm_id  = keycloak_realm.mdc.id
+  client_id = keycloak_openid_client.emd_pagopa_mdc_pagopa_client.id
+
+  default_scopes = [
+    "openid",
+    "profile",
+    keycloak_openid_client_scope.mdc_base_claims.name,
+  ]
+}
+
 # create emd-tpp-test client
 resource "keycloak_openid_client" "emd_pagopa_mdc_emd_tpp_test_client" {
   realm_id = keycloak_realm.mdc.id
@@ -52,6 +114,18 @@ resource "keycloak_openid_client" "emd_pagopa_mdc_emd_tpp_test_client" {
   access_type              = "CONFIDENTIAL"
   service_accounts_enabled = true
 
+}
+
+# Emd-tpp-test client default scopes
+resource "keycloak_openid_client_default_scopes" "emd_pagopa_mdc_emd_tpp_test_client_default_scopes" {
+  realm_id  = keycloak_realm.mdc.id
+  client_id = keycloak_openid_client.emd_pagopa_mdc_emd_tpp_test_client.id
+
+  default_scopes = [
+    "openid",
+    "profile",
+    keycloak_openid_client_scope.mdc_base_claims.name,
+  ]
 }
 
 # Send group
