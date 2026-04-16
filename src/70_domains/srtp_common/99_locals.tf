801feb40588f98f6295150915e57b0e25909157c
ffppa <fabio.felici@pagopa.it>
2026-04-16 16:52:43 +0200

    removed permission for production

--- src/70_domains/srtp_common/99_locals.tf
+++ src/70_domains/srtp_common/99_locals.tf
@@ -175,11 +175,11 @@ locals {
   }
 
   ad_group_rbac = flatten([
-    {
-      object_id    = data.azuread_group.adgroup_domain_admin.object_id
-      display_name = data.azuread_group.adgroup_domain_admin.display_name
-    },
     var.env_short != "p" ? [
+      {
+        object_id    = data.azuread_group.adgroup_domain_admin.object_id
+        display_name = data.azuread_group.adgroup_domain_admin.display_name
+      },
       {
         object_id    = data.azuread_group.adgroup_domain_developers.object_id
         display_name = data.azuread_group.adgroup_domain_developers.display_name
@@ -187,14 +187,12 @@ locals {
       {
         object_id    = data.azuread_group.adgroup_domain_externals.object_id
         display_name = data.azuread_group.adgroup_domain_externals.display_name
-      }
-    ] : [],
-    var.env_short == "p" ? [
+      },
       {
         object_id    = data.azuread_group.adgroup_domain_oncall[0].object_id
         display_name = data.azuread_group.adgroup_domain_oncall[0].display_name
       }
-    ] : []
+    ] : [],
   ])
 
 }
