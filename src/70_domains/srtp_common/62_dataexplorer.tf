9f04227c28a5d47a33285051c08ceadbab597e05
davideragusi29 <davide.ragusi@digitouch.it>
2026-04-17 17:41:26 +0200

    fix: [SRTP-1703] correct workload identity and ADX principal assignment for rtp-sender (#600)
    * fix: [SRTP-1703] update client_id
    
    * fix: [SRTP-1703] update rtp-role-viewer
    
    ---------
    
    Co-authored-by: Davide Ragusi <davideragusi@Davides-MacBook-Pro.local>

--- src/70_domains/srtp_common/62_dataexplorer.tf
+++ src/70_domains/srtp_common/62_dataexplorer.tf
@@ -50,12 +50,12 @@ resource "azurerm_data_factory_linked_service_kusto" "kusto" {
 }
 
 resource "azurerm_kusto_database_principal_assignment" "rtp_sender_adx_viewer" {
-  name                = "rtp-sender-viewer"
+  name                = "rtp-role-viewer"
   resource_group_name = data.azurerm_kusto_cluster.kusto_cluster.resource_group_name
   cluster_name        = data.azurerm_kusto_cluster.kusto_cluster.name
   database_name       = azurerm_kusto_database.db[var.domain].name
 
-  principal_id   = data.azurerm_user_assigned_identity.rtp_sender_workload_identity.principal_id
+  principal_id   = data.azurerm_user_assigned_identity.rtp_sender_workload_identity.client_id
   principal_type = "App"
   tenant_id      = data.azurerm_client_config.current.tenant_id
   role           = "Viewer"
--- src/70_domains/srtp_common/99_locals.tf
+++ src/70_domains/srtp_common/99_locals.tf
@@ -175,7 +175,8 @@ locals {
   }
 
   # Workload Identity
-  rtp_sender_workload_identity_name = "${local.project}-sender-id"
+  rtp_sender_workload_identity_name    = "${var.domain}-${var.location_short}-workload-identity"
+  rtp_sender_workload_identity_rg_name = local.aks_resource_group_name
 
   ad_group_rbac = flatten([
     var.env_short != "p" ? [
