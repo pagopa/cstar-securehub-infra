f4a4ba72f97bda9b8ae22865a17cb8955d37617a
Davide Ragusi <davideragusi@Davides-MacBook-Pro.local>
2026-04-17 12:44:16 +0200

    fix: [SRTP-1703] update client_id

--- src/70_domains/srtp_common/00_data.tf
+++ src/70_domains/srtp_common/00_data.tf
@@ -168,5 +168,5 @@ data "azurerm_kusto_cluster" "kusto_cluster" {
 #
 data "azurerm_user_assigned_identity" "rtp_sender_workload_identity" {
   name                = local.rtp_sender_workload_identity_name
-  resource_group_name = local.identity_rg_name
+  resource_group_name = local.rtp_sender_workload_identity_rg_name
 }
--- src/70_domains/srtp_common/62_dataexplorer.tf
+++ src/70_domains/srtp_common/62_dataexplorer.tf
@@ -55,7 +55,7 @@ resource "azurerm_kusto_database_principal_assignment" "rtp_sender_adx_viewer" {
   cluster_name        = data.azurerm_kusto_cluster.kusto_cluster.name
   database_name       = azurerm_kusto_database.db[var.domain].name
 
-  principal_id   = data.azurerm_user_assigned_identity.rtp_sender_workload_identity.principal_id
+  principal_id   = data.azurerm_user_assigned_identity.rtp_sender_workload_identity.client_id
   principal_type = "App"
   tenant_id      = data.azurerm_client_config.current.tenant_id
   role           = "Viewer"
