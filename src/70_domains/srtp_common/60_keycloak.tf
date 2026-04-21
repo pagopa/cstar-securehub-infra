ae3ec612ddb75f616237c3f3914fb78f6a699291
luigidemarco-ship-it <luigi.demarco@pagopa.it>
2026-04-21 15:18:50 +0200

    new keycloak role

--- src/70_domains/srtp_common/60_keycloak.tf
+++ src/70_domains/srtp_common/60_keycloak.tf
@@ -58,6 +58,12 @@ resource "keycloak_role" "read_rtp_send" {
   description = "Role to read RTPs"
 }
 
+resource "keycloak_role" "read-rtp-payees-consents" {
+  realm_id    = keycloak_realm.srtp.id
+  name        = "read-rtp-payees-consents"
+  description = "Role to read RTPs Consents Information"
+}
+
 resource "keycloak_role" "read_rtp_all" {
   realm_id    = keycloak_realm.srtp.id
   name        = "read_rtp_all"
