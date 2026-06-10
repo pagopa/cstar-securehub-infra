18553b76f083512b8616da050d0df28081f71a9c
Antonio Tarricone <antonio.tarricone@pagopa.it>
2026-06-10 10:19:05 +0200

    Remove unused variables and add new ones.

--- src/91_github/idpay/99_variables.tf
+++ src/91_github/idpay/99_variables.tf
@@ -1,34 +1,19 @@
-variable "prefix" {
-  type = string
-  validation {
-    condition = (
-      length(var.prefix) <= 6
-    )
-    error_message = "Max length is 6 chars."
-  }
-}
-
-variable "env_short" {
-  type = string
-  validation {
-    condition = (
-      length(var.env_short) <= 1
-    )
-    error_message = "Max length is 1 chars."
-  }
-}
-
-variable "location_short" {
-  type        = string
-  description = "Location short like eg: neu, weu.."
-}
-
-variable "domain" {
-  type        = string
-  description = "Domain used for tags."
-}
-
 variable "datavault_service_url_for_uat" {
   type        = string
   description = "URL of the datavault service for UAT environment."
+}
+
+variable "subscription_id_for_uat" {
+  type        = string
+  description = "Subscription ID of the UAT environment, retrieved dynamically."
+}
+
+variable "argocd_server_for_prod" {
+  type        = string
+  description = "Server of the ArgoCD server for PROD environment."
+}
+
+variable "argocd_server_for_uat" {
+  type        = string
+  description = "Server of the ArgoCD server for UAT environment."
 }
