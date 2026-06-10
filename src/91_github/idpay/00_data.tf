c9cbfd285bafa4bc3191ff596d40433545ed4027
Antonio Tarricone <antonio.tarricone@pagopa.it>
2026-06-10 10:20:17 +0200

    Add reference to KV for UAT and references to new secrets.

--- src/91_github/idpay/00_data.tf
+++ src/91_github/idpay/00_data.tf
@@ -2,12 +2,38 @@ data "github_team" "admin" {
   slug = "idpay-approver-team"
 }
 
-data "azurerm_key_vault" "core_cicd" {
-  name                = "${var.prefix}-${var.env_short}-${var.location_short}-cicd-kv"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-core-sec-rg"
+data "azurerm_key_vault" "core_cicd_for_prod" {
+  name                = "cstar-p-itn-cicd-kv"
+  resource_group_name = "cstar-p-itn-core-sec-rg"
+}
+
+data "azurerm_key_vault" "uat_core_cicd_for_uat" {
+  name                = "cstar-u-itn-cicd-kv"
+  resource_group_name = "cstar-u-itn-core-sec-rg"
+  provider            = azurerm.uat
 }
 
 data "azurerm_key_vault_secret" "sonar_token" {
-  key_vault_id = data.azurerm_key_vault.core_cicd.id
+  key_vault_id = data.azurerm_key_vault.core_cicd_for_prod.id
   name         = "sonar-token"
+}
+
+data "azurerm_key_vault_secret" "argocd_admin_username_for_prod" {
+  key_vault_id = data.azurerm_key_vault.core_cicd_for_prod.id
+  name         = "argocd-admin-username"
+}
+
+data "azurerm_key_vault_secret" "argocd_admin_password_for_prod" {
+  key_vault_id = data.azurerm_key_vault.core_cicd_for_prod.id
+  name         = "argocd-admin-password"
+}
+
+data "azurerm_key_vault_secret" "argocd_admin_username_for_uat" {
+  key_vault_id = data.azurerm_key_vault.uat_core_cicd_for_uat.id
+  name         = "argocd-admin-username"
+}
+
+data "azurerm_key_vault_secret" "argocd_admin_password_for_uat" {
+  key_vault_id = data.azurerm_key_vault.uat_core_cicd_for_uat.id
+  name         = "argocd-admin-password"
 }
