9387b3a8e2aff44cd29713ae4280aa82cbef881a
fabio.picano <fabio.picano@nttdata.com>
2026-07-13 16:01:30 +0200

    remove comments

--- src/10_networking/21_private_network.tf
+++ src/10_networking/21_private_network.tf
@@ -46,17 +46,11 @@ resource "azurerm_private_dns_zone_virtual_network_link" "file_private_endpoint_
 
 resource "azurerm_private_dns_zone" "web_storage" {
   name = "privatelink.web.core.windows.net"
-  # resource_group_name = data.azurerm_virtual_network.vnet_weu_integration.resource_group_name
   resource_group_name = azurerm_resource_group.rg_network.name
   tags                = module.tag_config.tags
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "web_storage" {
-  # name                  = "web-storage-private-dns-link"
-  # resource_group_name   = data.azurerm_virtual_network.vnet_weu_integration.resource_group_name
-  # private_dns_zone_name = azurerm_private_dns_zone.web_storage.name
-  # virtual_network_id = data.azurerm_virtual_network.vnet_spoke_data.id
-
   for_each = { for i in local.vnets_all : i.name => i }
 
   name                  = "${each.key}-web-storage-link"
