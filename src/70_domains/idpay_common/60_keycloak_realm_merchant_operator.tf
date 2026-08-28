62049d7e819dbc0f16d741b9fe75c8bad93bec3b
and-mora <andrea.morabito@pagopa.it>
2026-08-28 08:50:17 +0200

    fix: update Keycloak functional test client count condition to use shorthand for environment check

--- src/70_domains/idpay_common/60_keycloak_realm_merchant_operator.tf
+++ src/70_domains/idpay_common/60_keycloak_realm_merchant_operator.tf
@@ -255,13 +255,13 @@ resource "keycloak_openid_client_default_scopes" "merchant_frontend_defaults" {
 
 ### Client for functional testing on payments, only in DEV and UAT environments, not in PROD
 resource "random_password" "keycloak_merchant_operator_functional_test_client" {
-  count   = var.env_short != "prod" ? 1 : 0
+  count   = var.env_short != "p" ? 1 : 0
   length  = 30
   special = false
 }
 
 resource "azurerm_key_vault_secret" "keycloak_merchant_operator_functional_test_client_secret" {
-  count        = var.env_short != "prod" ? 1 : 0
+  count        = var.env_short != "p" ? 1 : 0
   name         = "keycloak-merchant-op-functional-test-client-secret"
   value        = random_password.keycloak_merchant_operator_functional_test_client[0].result
   key_vault_id = data.azurerm_key_vault.domain_kv.id
@@ -271,7 +271,7 @@ resource "azurerm_key_vault_secret" "keycloak_merchant_operator_functional_test_
 
 
 resource "keycloak_openid_client" "merchant_operator_functional_test_client" {
-  count    = var.env_short != "prod" ? 1 : 0
+  count    = var.env_short != "p" ? 1 : 0
   realm_id = module.keycloak_setup.realm_ids["merchant-operator"]
   name     = "Merchant Op Functional Test Client"
   enabled  = true
@@ -287,7 +287,7 @@ resource "keycloak_openid_client" "merchant_operator_functional_test_client" {
 }
 
 resource "keycloak_openid_client_default_scopes" "merchant_operator_functional_test_defaults" {
-  count     = var.env_short != "prod" ? 1 : 0
+  count     = var.env_short != "p" ? 1 : 0
   realm_id  = module.keycloak_setup.realm_ids["merchant-operator"]
   client_id = keycloak_openid_client.merchant_operator_functional_test_client[0].id
 
