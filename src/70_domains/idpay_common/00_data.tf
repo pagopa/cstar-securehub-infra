2d6c4566be0ef57fc836deb2c59daefd730e0f44
stedelia <144045955+stedelia@users.noreply.github.com>
2026-09-17 13:37:45 +0000

    fix: update ses pari email on DEV and UAT (#785)

--- src/70_domains/idpay_common/00_data.tf
+++ src/70_domains/idpay_common/00_data.tf
@@ -191,6 +191,26 @@ data "azurerm_key_vault_secret" "ses_from_address" {
   name         = "aws-ses-mail-from"
   key_vault_id = data.azurerm_key_vault.domain_kv.id
 }
+#PARI SES
+data "azurerm_key_vault_secret" "pari_ses_smtp_username" {
+  name         = "aws-pari-ses-mail-smtp-username"
+  key_vault_id = data.azurerm_key_vault.domain_kv.id
+}
+
+data "azurerm_key_vault_secret" "pari_ses_smtp_password" {
+  name         = "aws-pari-ses-mail-smtp-password"
+  key_vault_id = data.azurerm_key_vault.domain_kv.id
+}
+
+data "azurerm_key_vault_secret" "pari_ses_smtp_host" {
+  name         = "aws-pari-ses-mail-host"
+  key_vault_id = data.azurerm_key_vault.domain_kv.id
+}
+
+data "azurerm_key_vault_secret" "pari_ses_from_address" {
+  name         = "aws-pari-ses-mail-from"
+  key_vault_id = data.azurerm_key_vault.domain_kv.id
+}
 
 data "azurerm_key_vault_secret" "oneidentity-client-id" {
   name         = "oneidentity-client-id"
