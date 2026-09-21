220c53914f4c542a8faa68ddd1257f02a0e25443
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2026-09-21 16:06:02 +0200

    feat: SRTP-2168 update alert query to filter by AppRoleName for rtp-sender

--- src/70_domains/srtp_app/12_alerts_locals.tf
+++ src/70_domains/srtp_app/12_alerts_locals.tf
@@ -132,9 +132,11 @@ locals {
       location       = data.azurerm_log_analytics_workspace.log_analytics_workspace.location
       query          = <<-QUERY
             let excluded_requests = AppTraces
+              | where AppRoleName == "rtp-sender"
               | where Message has "Size.gpdMessageDtoMono.description" or Message has "Size.gpdMessageDtoMono.subject"
               | distinct OperationId;
             AppRequests
+            | where AppRoleName == "rtp-sender"
             | where Url contains "rtp/gpd/message"
             | where Name startswith "POST"
             | join kind=leftanti excluded_requests on OperationId
