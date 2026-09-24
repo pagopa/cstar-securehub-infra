5e47d3fc5bef463ea8d7c8712f81ed7d6e3535f2
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2026-09-24 13:08:08 +0200

    feat: Add data collection rule for log analytics workspace (#798)
    * feat: Add data collection rule for log analytics workspace
    
    * align tfstate

--- src/31_aks_italy/02_aks.tf
+++ src/31_aks_italy/02_aks.tf
@@ -7,7 +7,6 @@ resource "azurerm_resource_group" "aks_rg" {
 
 module "aks" {
   source = "./.terraform/modules/__v4__/kubernetes_cluster"
-  # source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//kubernetes_cluster?ref=aks-ignore-workload-autoscale"
 
   name                       = local.aks_name
   location                   = var.location
--- src/31_aks_italy/02_aks_node_pools.tf
+++ src/31_aks_italy/02_aks_node_pools.tf
@@ -22,7 +22,7 @@ module "aks_user_node_pool_blue" {
   kubernetes_cluster_id = module.aks.id
   vnet_subnet_id        = module.aks_user_snet.id
 
-  autoscale_enabled = false
+  autoscale_enabled = var.aks_nodepool_blue.autoscale_enabled
 }
 
 module "aks_user_node_pool_green" {
